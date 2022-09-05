249eb1c316eb2ef079f9a8a3f1e2d5972724783a
Trang Nguyen <ntttrang2130@gmail.com>
2022-09-04 23:44:58 -0400

    graph undirected-path visited

--- graph/undirected-path.js
+++ graph/undirected-path.js
@@ -2,33 +2,36 @@
 
 const undirectedPath = (edges, nodeA, nodeB) => {
   const graph = buildGraph(edges);
-  return hasPath(graph, nodeA, nodeB);
+  return hasPath(graph, nodeA, nodeB, new Set());
 };
-
-const hasPath = (graph, src, dst) => {
-  if (src === dst) return true;
-
-  for (let neighbor of graph[src]) {
-    if (hasPath(graph, neighbor, dst) === true) {
-      return true;
-    }
-  }
-  return false;
-}
 
 const buildGraph = (edges) => {
   const graph = {};
 
   for (let edge of edges) {
-    const [ a, b] = edge;
+    const [ a, b ] = edge;
     if (!(a in graph)) graph[a] = [];
     if (!(b in graph)) graph[b] = [];
-
     graph[a].push(b);
     graph[b].push(a);
   }
-  return graph
-}
+
+  return graph;
+};
+
+const hasPath = (graph, src, dst, visited) => {
+  if (src === dst) return true;
+  if (visited.has(src)) return false;
+  visited.add(src);
+
+  for (let neighbor of graph[src]) {
+    if (hasPath(graph, neighbor, dst, visited) === true) {
+      return true;
+    }
+  }
+
+  return false;
+};
 /**test_00:
 const edges = [
   ['i', 'j'],
