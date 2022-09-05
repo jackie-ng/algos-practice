3f93d730a738b1e9cf7d2af7424ba6ff6026cf92
Trang Nguyen <ntttrang2130@gmail.com>
2022-09-05 00:28:11 -0400

    largest component + visited

--- graph/largest-component.js
+++ graph/largest-component.js
@@ -1,20 +1,25 @@
 /**Write a function, largestComponent, that takes in the adjacency list of an undirected graph. The function should return the size of the largest connected component in the graph. */
 
 const largestComponent = (graph) => {
+  const visited = new Set();
   const largest = 0;
 
   for (let node in graph) {
-    const size = exploreSize(graph, node);
+    const size = exploreSize(graph, node, visited);
     if (size > largest) largest = size;
   }
   return largest;
 }
 
-const exploreSize = (graph, node) => {
+const exploreSize = (graph, node, visited) => {
+  if (visited.has(node)) return 0;
+
+  visited.add(node);
+
   let size = 1;
 
   for (let neighbor of graph[node]) {
-    size += exploreSize(graph, neighbor);
+    size += exploreSize(graph, neighbor, visited);
   }
   return size;
 }
