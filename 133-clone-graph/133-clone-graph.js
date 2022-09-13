1f1855b5a849878344cceff32909c50e5afef775
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2022-09-12 23:41:46 -0400

    Time: 102 ms (39.04%), Space: 43.8 MB (68.65%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.js
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, neighbors) {
+ *    this.val = val === undefined ? 0 : val;
+ *    this.neighbors = neighbors === undefined ? [] : neighbors;
+ * };
+ */
+
+/**
+ * @param {Node} node
+ * @return {Node}
+ */
+
+/*https://leetcode.com/problems/clone-graph/discuss/1416436/Javascript-DFS-%2B-Hashmap-(Thought-Process)*/
+
+var cloneGraph = function(node, copy = new Map()) {
+    // Handle null case
+    if (!node) return null;
+        
+    // If our graph hasn't already copied the input node
+    if (!copy.has(node.val)) {
+        // Create new copy node (+ reference in map)
+        copy.set(node.val, new Node(node.val));
+        // Recursively clone neighbors
+        copy.get(node.val).neighbors = node.neighbors.map((neighbor) => cloneGraph(neighbor, copy));
+    }
+        
+    // Return copied/new node (* not * the original node passed in)
+    return copy.get(node.val);
+};
