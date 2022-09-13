21f611e0a04ba82e184a2d44872b94a761e111c7
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2022-09-13 00:43:25 -0400

    Time: 822 ms (42.89%), Space: 156.5 MB (56.17%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.js
@@ -6,35 +6,47 @@
  * @return {boolean}
  */
 
-var validPath = function(n, edges, source, destination) {
-    const graph = buildGraph(edges)
-    return hasPath(graph, source, destination, new Set())
+var validPath = function(n, edges, start, end) {
+    // Create a hashmap to be used as our adjacency list
+    const graph = new Map();
+    
+    // Create a set to store our visited nodes
+    const visited = new Set();
+    
+    // Build adjacency list (undirected)
+    for (const [v, e] of edges) {
+        if (graph.has(v)) {
+            graph.get(v).push(e);
+        } else {
+            graph.set(v, [e]);
+        }
+        
+        if (graph.has(e)) {
+            graph.get(e).push(v);
+        } else {
+            graph.set(e, [v]);
+        }
+    }
+        
+    // Define a recursive DFS helper
+    function dfs(v) {
+        // Add to visited set
+        visited.add(v);
+                
+        // Get adjacent vertices
+        const edges = graph.get(v);
+                
+        // For all adjacent vertices, run DFS
+        if (edges && edges.length > 0) {
+            for (const e of edges) {
+                if (!visited.has(e)) dfs(e);
+            }
+        }
+    }
+        
+    // DFS from starting input node
+    dfs(start);
+        
+    // Return true/false if our visited set has our end node
+    return visited.has(end);
 };
-
-function hasPath(graph, src, dst, visited) {
-    if (src === dst) return true
-    if (visited.has(src)) return false
-    
-    visited.add(src)
-    
-    for (let neighbor of graph[src]) {
-        if (hasPath(graph, neighbor, dst, visited))
-            return true
-    }
-    
-    return false
-}
-
-function buildGraph(edges) {
-    const graph = {}
-    
-    for (const [a, b] of edges) {
-        if (!(a in graph)) graph[a] = []
-        if (!(b in graph)) graph[b] = []
-        graph[a].push(b)
-        graph[b].push(a)
-    }
-    
-    return graph
-
-}
