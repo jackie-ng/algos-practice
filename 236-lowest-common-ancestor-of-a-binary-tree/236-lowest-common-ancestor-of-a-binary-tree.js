dc3e03e719064751d40006609deecd5fe1c2c7cf
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2022-09-19 20:37:29 -0400

    Time: 115 ms (60.71%), Space: 51.9 MB (42.73%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.js
@@ -11,9 +11,11 @@
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-function lowestCommonAncestor(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  var resL = lowestCommonAncestor(root.left, p, q);
-  var resR = lowestCommonAncestor(root.right, p, q);
-  return (resL && resR) ? root : (resL || resR);
-}
+const lowestCommonAncestor = (root, p, q) => {
+    if (!root || root === p || root === q) return root
+    const left = lowestCommonAncestor(root.left, p, q)
+    const right = lowestCommonAncestor(root.right, p, q)
+    if (!left) return right  // p and q are in the right subtree
+    if (!right) return left  // p and q are in the left subtree
+    return root              // p is in one side and q is in the other
+};
