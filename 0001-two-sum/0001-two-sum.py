a1831ae42b45591e18deee20a5cbb10c860b1cba
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-04-22 19:08:55 -0400

    Time: 71 ms (46.32%), Space: 14.9 MB (89.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,39 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         hashmap = {}
+#         for i in range(len(nums)):
+#             hashmap[nums[i]] = i
+#         for i in range(len(nums)):
+#             complement = target - nums[i]
+#             if complement in hashmap and hashmap[complement] != i:
+#                 return [i, hashmap[complement]] 
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        for i in range(len(nums)):
-            complement = target - nums[i]
-            if complement in hashmap and hashmap[complement] != i:
-                return [i, hashmap[complement]] 
+    def twoSum(self, nums: List[int], target: int) -> List[int]:        
+        n = len(nums)
+        a = nums.copy()
+
+        nums.sort()
+
+        l = 0
+        r = n - 1
+
+        while l < r:
+            sum = nums[l] + nums[r]
+
+            if sum == target:
+                break
+            elif sum > target:
+                r -= 1
+            else:
+                l += 1
+
+        v = []
+
+        for i in range(n):
+            if nums[l] == a[i]:
+                v.append(i)
+            elif nums[r] == a[i]:
+                v.append(i)
+
+        return v
