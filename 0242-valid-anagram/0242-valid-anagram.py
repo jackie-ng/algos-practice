7e237b062c4ddf99fbc63c43d05083b1f685fe02
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-08-31 15:32:01 -0400

    Time: 57 ms (62.70%), Space: 16.8 MB (86.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -5,16 +5,39 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
+        # build 2 hash maps
         if len(s) != len(t):
             return False
         
-        freq = [0] * 26
+        countS, countT = {}, {}
+        
         for i in range(len(s)):
-            freq[ord(s[i]) - ord('a')] += 1
-            freq[ord(t[i]) - ord('a')] -= 1
+            countS[s[i]] = 1 + countS.get(s[i], 0) #0 is a default value if s[i] does not exist in the hashmap
+            countT[t[i]] = 1 + countT.get(t[i], 0)
         
-        for i in range(len(freq)):
-            if freq[i] != 0:
+        for j in countS:
+            if countS[j] != countT.get(j, 0):
                 return False
-        
         return True
+            
+#         freq = [0] * 26
+#         for i in range(len(s)):
+#             freq[ord(s[i]) - ord('a')] += 1
+#             freq[ord(t[i]) - ord('a')] -= 1
+        
+#         for i in range(len(freq)):
+#             if freq[i] != 0:
+#                 return False
+        
+#         return True
+    
+#         for c in countS:
+#             if countS[c] != countT[c]:
+#                 return False
+            
+#         return True
+
+           
+    
+    
+    
