2faa9e7c432cde09620623bc155d77682ebb1c67
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-09-06 20:18:48 -0400

    Time: 114 ms (33.93%), Space: 22.4 MB (8.10%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # mapping charCount to list of Anagrams
+        res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
         
         for s in strs:
             count = [0] * 26 # a ... z
