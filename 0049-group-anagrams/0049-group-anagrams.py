65f2facb5791a27124458016d18a910556c8ecb5
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-09-06 20:42:52 -0400

    Time: 109 ms (45.74%), Space: 19.9 MB (65.50%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,27 @@
-from collections import defaultdict
+# from collections import defaultdict
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
+#         print(res)
+#         for s in strs:
+#             count = [0] * 26 # a ... z
+            
+#             for c in s:
+#                 count[ord(c) - ord("a")] += 1
+            
+#             # print(res[tuple(count)], "s:", s)
+#             res[tuple(count)].append(s)
+            
+#         return res.values()
+    
+    def groupAnagrams(self, strs):
+        d = {}
         
-        for s in strs:
-            count = [0] * 26 # a ... z
-            
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-                
-            res[tuple(count)].append(s)
-            
-        return res.values()
+        for i in range(len(strs)):
+            x = ''.join(sorted(strs[i]))
+            if x not in d:
+                d[x] = [strs[i]]
+            else:
+                d[x].append(strs[i])
+        return d.values()
