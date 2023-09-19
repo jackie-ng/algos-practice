c723ba13ea5c1345f5e8b72481d21360f0809f9f
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-09-19 13:51:25 -0400

    Time: 75 ms (20.07%), Space: 20.3 MB (73.69%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -5,36 +5,47 @@ class MinStack:
         self.min_stack = []
         
     def push(self, val: int) -> None:
-        self.stack.append((val))
-        # if the min stack is empty, or this number is smaller than
-        # the top of the min stack, put it on with a count of 1
-        if not self.min_stack or val < self.min_stack[-1][0]:
-            self.min_stack.append([val, 1])
-        # Else if this number is equal to what's currently at the top of the min stack,
-        # then increment the count at the top by 1
-        elif val == self.min_stack[-1][0]:
-            self.min_stack[-1][1] += 1
+        #####Shorter solution####
+        self.stack.append(val)
+        val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(val)
+        
+#         self.stack.append((val))
+#         # if the min stack is empty, or this number is smaller than
+#         # the top of the min stack, put it on with a count of 1
+#         if not self.min_stack or val < self.min_stack[-1][0]:
+#             self.min_stack.append([val, 1])
+#         # Else if this number is equal to what's currently at the top of the min stack,
+#         # then increment the count at the top by 1
+#         elif val == self.min_stack[-1][0]:
+#             self.min_stack[-1][1] += 1
 
+
+        
     def pop(self) -> None:
+        #####Shorter solution####
+        self.stack.pop()
+        self.min_stack.pop()
+        
         # if the top if min stack is the same as the top of the stack
         # then we need to decrement the count at the top by 1
-        if self.min_stack[-1][0] == self.stack[-1]:
-            self.min_stack[-1][1] -= 1
+#         if self.min_stack[-1][0] == self.stack[-1]:
+#             self.min_stack[-1][1] -= 1
         
-        # if the count at the top of min stack is now 0,
-        # then remove that value as we're done with it
-        if self.min_stack[-1][1] == 0:
-            self.min_stack.pop()
+#         # if the count at the top of min stack is now 0,
+#         # then remove that value as we're done with it
+#         if self.min_stack[-1][1] == 0:
+#             self.min_stack.pop()
             
-        # Pop the top of the main stack
-        self.stack.pop()
+#         # Pop the top of the main stack
+#         self.stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
 
     def getMin(self) -> int:
-        return self.min_stack[-1][0]   
+        return self.min_stack[-1]
         
 
 
