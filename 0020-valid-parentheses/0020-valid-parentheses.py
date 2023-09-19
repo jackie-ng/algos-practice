8a9e366b887e9b04277f0add6b3ecb950042e3ab
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-09-19 12:48:52 -0400

    Time: 48 ms (12.26%), Space: 16.4 MB (47.37%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,13 +4,12 @@ class Solution:
         closeToOpen = {")": "(", "]": "[", "}": "{"}
         
         for char in s:
-            if char in closeToOpen: #is a closing parentheses
+            if char in closeToOpen:
                 if stack and stack[-1] == closeToOpen[char]:
                     stack.pop()
                 else:
                     return False
             else:
                 stack.append(char)
-        
         return True if not stack else False
-                
+        
