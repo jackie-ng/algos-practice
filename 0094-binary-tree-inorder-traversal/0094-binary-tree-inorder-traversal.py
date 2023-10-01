400e4783e6ecb439ccb745b2959156f8eb986b3d
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-01 04:28:49 -0400

    Time: 29 ms (96.28%), Space: 16.1 MB (88.50%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -6,13 +6,37 @@
 #         self.right = right
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        res = []
-        def inOrder(node):
-            if node is None:
-                return
-            inOrder(node.left)
-            res.append(node.val)
-            inOrder(node.right)
+#         res = []
+#         def inOrder(node):
+#             if node is None:
+#                 return
+#             inOrder(node.left)
+#             res.append(node.val)
+#             inOrder(node.right)
                 
-        inOrder(root)
+#         inOrder(root)
+#         return res
+#         res = []
+#         stack = [root]
+        
+#         while stack:
+#             curr_node = stack.pop()
+#             if curr_node:
+#                 stack.append(curr_node.right)
+#                 res.append(curr_node.val)
+#                 stack.append(curr_node.left)
+                
+#         return res
+    
+
+        res, stack = [], [(root, False)]
+        while stack:
+            node, visited = stack.pop()  # the last element
+            if node:
+                if visited:
+                    res.append(node.val)
+                else:  # inorder: left -> root -> right
+                    stack.append((node.right, False))
+                    stack.append((node, True))
+                    stack.append((node.left, False))
         return res
