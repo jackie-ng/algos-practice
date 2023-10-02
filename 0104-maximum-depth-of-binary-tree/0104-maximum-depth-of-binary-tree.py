dbeea6c8fc631dbc5dff04e2a7b993a5f065a37d
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-02 17:50:53 -0400

    Time: 55 ms (6.09%), Space: 17.5 MB (95.17%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.py
@@ -6,7 +6,46 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
+#         if not root:
+#             return 0
         
-        return max(1 + self.maxDepth(root.left), 1 + self.maxDepth(root.right))
+#         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+#         ##### BFS #####    
+#        if not root:
+#            return 0
+#         level = 0
+#         q = collections.deque([root])
+        
+#         while q:
+#             #traverse through the level
+#             for i in range(len(q)):
+#                 #pop q node
+#                 node = q.popleft()
+#                 #check the children of this node, add the children to the queue if it's not null
+#                 if node.left:
+#                     q.append(node.left)
+#                 if node.right:
+#                     q.append(node.right)
+                
+                
+#             # after being done with the level traversal, add 1 to the level
+#             level += 1
+#         return level
+
+        ##### DFS #####
+            if not root:
+                return 0
+            stack = [[root, 1]]
+            res = 1
+            
+            while stack:
+                node, depth = stack.pop()
+                
+                if node:
+                    res = max(res, depth) 
+                    stack.append([node.left, depth + 1])
+                    stack.append([node.right, depth + 1])
+            return res
+                    
