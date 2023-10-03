acf6a40bc29da56ce2149e6aebc5df85513e7bad
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-03 14:42:19 -0400

    Time: 166 ms (6.43%), Space: 20.7 MB (99.55%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -7,7 +7,7 @@ class Solution:
         # 1. build hash map: character and how often it appears 
         #     O(N) time
         count = Counter(nums)
-        # 2-3. build heal of top k frequent elements and convert it into an output array
+        # 2-3. build heap of top k frequent elements and convert it into an output array
         # O(N log k) time
         return heapq.nlargest(k, count.keys(), key=count.get)
         
