cc8bc1f6d193d21146942109f72358c523abc824
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-02 20:55:50 -0400

    Time: 44 ms (93.37%), Space: 21.2 MB (42.83%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -4,11 +4,17 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isBalanced(self, root):
-        return (self.Height(root) >= 0)
-    def Height(self, root):
-        if root is None:  return 0
-        leftheight, rightheight = self.Height(root.left), self.Height(root.right)
-        if leftheight < 0 or rightheight < 0 or abs(leftheight - rightheight) > 1:  return -1
-        return max(leftheight, rightheight) + 1
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        
+        def dfs(root):
+            if not root: 
+                return [True, 0]
+            # determine if left and right subtree are balance
+            left, right = dfs(root.left), dfs(root.right)
+            # balanced if the left subtree, the right subtree and the root are balanced
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+            
+            return [balanced, 1 + max(left[1], right[1])]
+        
+        return dfs(root)[0]
