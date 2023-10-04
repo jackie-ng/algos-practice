42fd1ab1b8a9eac37dc8895c85117ee1536bac48
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-04 14:59:16 -0400

    Time: 5027 ms (5.00%), Space: 31.2 MB (53.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
+        print(numSet)
         longest = 0
         
         for n in nums:
@@ -33,21 +34,3 @@ class Solution:
 
 #         return max(longest_streak, current_streak)
 
-# # HashSet and Intelligent Sequence Building
-# class Solution:
-#     def longestConsecutive(self, nums):
-#         longest_streak = 0
-#         num_set = set(nums)
-
-#         for num in num_set:
-#             if num - 1 not in num_set:
-#                 current_num = num
-#                 current_streak = 1
-
-#                 while current_num + 1 in num_set:
-#                     current_num += 1
-#                     current_streak += 1
-
-#                 longest_streak = max(longest_streak, current_streak)
-
-#         return longest_streak
