647308477e2cdf0331592a818916903653984dc4
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-04 21:01:43 -0400

    Time: 845 ms (19.24%), Space: 36.2 MB (84.24%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -1,19 +1,17 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        # 12
-        # position = [10,8,0,5,3]
-        # speed = [2,4,1,1,3]
-        # print(pair)
-        # ---> [[10, 2], [8, 4], [0, 1], [5, 1], [3, 3]]
-        pair = [[p, s] for p, s in zip(position, speed)] #list comprehension
         stack = []
+        # List comprehension
+        pairs = [[p, s] for p, s in zip(position, speed)]
         
-        for p, s in sorted(pair)[::-1]: # Reverse Sorted order
-             # time the car will reach the destination = (target - position) / speed
-            stack.append((target - p) / s)
-            # check if it's overlapped with the top of your stack
-            # check if the stack has at least 2 elements
-            # and if the top of the stack collied with the 2nd top of the stack => pop the top of the stack
+        for p, s in sorted(pairs)[::-1]:
+            time = (target - p) / s
+            stack.append(time)
+            
+            # Collision happens when:
+            # + There are at least 2 cars
+            # + Time to arrive dest of the behind car is <= the front car
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
+            
         return len(stack)
