3cd5fa838ed6067d521a4de68117e99e83bd71ce
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-04 21:25:57 -0400

    Time: 1067 ms (56.71%), Space: 33 MB (8.62%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,15 +1,13 @@
 class Solution:
-    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # Time: O(n)
-        # Space: O(n)
-        # Monotonic Stack
-        res = [0] * len(temperatures)
-        stack = [] # pair: [temp, index]
+    def dailyTemperatures(self, temp: List[int]) -> List[int]:
+        stack = [] # pair [temp, index]
+        res = [0] * len(temp)
         
-        for i, t in enumerate(temperatures):
+        for i, t in enumerate(temp):
             while stack and t > stack[-1][0]:
                 stackT, stackIdx = stack.pop()
                 res[stackIdx] = (i - stackIdx)
             stack.append([t, i])
-            
         return res
+        
+        
