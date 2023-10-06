276b1477798257c8d5cdad98991342d54fb18964
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-06 18:06:11 -0400

    Time: 56 ms (68.57%), Space: 20.4 MB (64.13%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,10 +6,22 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        nodes_seen = set()
-        while head:
-            if head in nodes_seen:
-                return True
-            nodes_seen.add(head)
-            head = head.next
-        return False
+        # hash = set()
+        # while head is not None:
+        #     if head in hash:
+        #         return True
+        #     hash.add(head)
+        #     head = head.next
+        # return False
+        if head is None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast is None or fast.next is None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        # slow == fast => there is a pointer
+        return True
