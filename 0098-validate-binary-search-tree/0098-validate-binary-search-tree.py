84d48c091d8b62f47f5e0ff6e6cefb924b286051
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-10 01:02:20 -0400

    Time: 51 ms (39.59%), Space: 19.3 MB (12.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,17 +4,43 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        # Valid Range: Recursion
-        def valid(node, left, right):
-            if not node:
-                return True
-            if not (node.val < right and node.val > left):
-                return False
+# class Solution:
+#     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+# #         # Valid Range: Recursion
+#         def valid(node, left, right):
+#             if not node:
+#                 return True
+#             if not (node.val < right and node.val > left):
+#                 return False
 
-            return (valid(node.left, left, node.val) and
-                    valid(node.right, node.val, right))
-        return valid(root, float("-inf"), float("inf"))
+#             return (valid(node.left, left, node.val) and
+#                     valid(node.right, node.val, right))
+#         return valid(root, float("-inf"), float("inf"))
             
-            
+            # Recursive: runtime-16ms
+
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        output =[]
+        self.inorder(root, output)
+        
+        for i in range(1, len(output)):
+            if output[i-1]>= output[i]:
+                return False
+        
+        return True
+    
+    # Time complexity of inorder traversal is O(n)
+    # Fun fact: Inorder traversal leads to a sorted array if it is 
+    # a Valid Binary Search. Tree.
+    def inorder(self, root, output):
+        if root is None:
+            return
+        
+        self.inorder(root.left, output)
+        output.append(root.val)
+        self.inorder(root.right, output)
