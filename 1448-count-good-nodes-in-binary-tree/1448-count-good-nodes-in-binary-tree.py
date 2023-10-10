2472aa3efec3cb0e6d55970243e8ea84134eab74
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-10 00:17:56 -0400

    Time: 207 ms (21.00%), Space: 35.1 MB (23.86%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -6,13 +6,27 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        def dfs(node, maxVal):
-            if not node:
-                return 0
-            res = 1 if node.val >= maxVal else 0
-            maxVal = max(maxVal, node.val)
-            res += dfs(node.left, maxVal)
-            res += dfs(node.right, maxVal)
-            return res
-        return dfs(root, root.val)
+        # def dfs(node, maxVal):
+        #     if not node:
+        #         return 0
+        #     res = 1 if node.val >= maxVal else 0
+        #     maxVal = max(maxVal, node.val)
+        #     res += dfs(node.left, maxVal)
+        #     res += dfs(node.right, maxVal)
+        #     return res
+        # return dfs(root, root.val)
+        #         num_good_nodes = 0
         
+        # Use collections.deque for efficient popping
+        num_good_nodes = 0
+        queue = collections.deque([(root, float("-inf"))])
+        while queue:
+            node, max_so_far = queue.popleft()
+            if max_so_far <= node.val:
+                num_good_nodes += 1
+            if node.left:
+                queue.append((node.left, max(node.val, max_so_far)))
+            if node.right:
+                queue.append((node.right, max(node.val, max_so_far)))
+        
+        return num_good_nodes
