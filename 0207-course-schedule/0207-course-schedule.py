5e14bbb83b4af7cdd4285bca84c1b3efba2d2af9
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-15 21:22:39 -0400

    Time: 98 ms (62.03%), Space: 19.2 MB (29.64%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,6 +1,6 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        preMap = { i:[] for i in range(numCourses) }
+        preMap = { i:[] for i in range(numCourses) } # each course set to empty list
         
         for crs, pre in prerequisites:
             preMap[crs].append(pre)
@@ -9,16 +9,18 @@ class Solution:
         visitSet = set()
         
         def dfs(crs):
+            ## base case ##
             if crs in visitSet: # detect a loop
                 return False
             if preMap[crs] == []: # this course has no prereq
                 return True
             
             visitSet.add(crs)
-            for pre in preMap[crs]:
-                if not dfs(pre): return False
-            visitSet.remove(crs)
-            preMap[crs] = []
+            for pre in preMap[crs]: 
+                if not dfs(pre): return False ## if 1 course cannot be completed => return False
+            
+            visitSet.remove(crs) #finish visiting => remove from visitSet
+            preMap[crs] = [] # to avoid repeated work since we checked it before
             return True
         
         for crs in range(numCourses):
