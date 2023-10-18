e2930e55a13b00ba7d86b0da12efdf46dd16e5e8
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-18 19:40:05 -0400

    Time: 95 ms (88.64%), Space: 16.4 MB (30.34%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -2,19 +2,20 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set) # key = (r/3, c/3)
+        squares = collections.defaultdict(set)
         
         for r in range(9):
             for c in range(9):
-                if board[r][c] == ".":
+                cur = board[r][c]
+                if cur == ".":
                     continue
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r//3, c//3)]):
+                if(cur in rows[r] or
+                   cur in cols[c] or
+                   cur in squares[(r // 3, c//3)]):
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[(r // 3, c // 3)].add(board[r][c])
+                cols[c].add(cur)
+                rows[r].add(cur)
+                squares[(r // 3, c//3)].add(cur)
         return True
     
 #             N = 9
