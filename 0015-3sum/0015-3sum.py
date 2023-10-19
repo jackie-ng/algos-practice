27c021584f7568f80ba8f86c476ce03d5eaeccbd
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-19 13:10:32 -0400

    Time: 953 ms (77.22%), Space: 20.2 MB (89.81%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,17 +3,17 @@ class Solution:
         res = []
         nums.sort()
         
-        for index, value in enumerate(nums): 
+        for index, value in enumerate(nums):
             # We don't want to reuse the same value in the same position twice
             # if it's not the first value in the input array and the value is equal to the value as before => continue to the next iteration for the remaining portion of the array 
             if index > 0 and value == nums[index - 1]:
                 continue
-            l, r = index + 1,  len(nums) - 1
-            while l < r: 
+            l, r = index + 1, len(nums) - 1
+            while l < r:
                 threeSum = value + nums[l] + nums[r]
-                if threeSum > 0:  
+                if threeSum > 0:
                     r -= 1
-                elif threeSum <0:
+                elif threeSum < 0:
                     l += 1
                 else:
                     res.append([value, nums[l], nums[r]])
@@ -23,4 +23,4 @@ class Solution:
                     l += 1
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
-        return res 
+        return res
