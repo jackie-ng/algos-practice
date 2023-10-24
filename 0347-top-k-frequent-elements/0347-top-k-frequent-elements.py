a34b75f6f39efabb66b6fe409396662cd34df938
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-23 20:16:03 -0400

    Time: 99 ms (57.78%), Space: 21.9 MB (15.84%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -12,22 +12,27 @@ class Solution:
         # return heapq.nlargest(k, count.keys(), key=count.get)
         
         
+        ########## Bucket Sort ############
         # i (count) 0 1 2 3 4 5 <--- go from the end of the freq array back. Freq = length of the input array
         # values     100  2 1    ---> 100 appear 1 time, 2 - 3 times, 1 - 4 times...
         
-        
         count = {}
         freq = [[] for i in range(len(nums) + 1)]
-        # print(freq)
+        
         for n in nums:
             count[n] = 1 + count.get(n, 0)
-        for n, c in count.items(): # every single key, value pair in our dictionary
-            freq[c].append(n) # value n occurs c times
-            # print(freq)
-            
+        for n, c in count.items():
+            freq[c].append(n)
+        
         res = []
-        for i in range(len(freq) - 1, 0, -1): # decensding order
-            for n in freq[i]:
-                res.append(n)
+        
+        for i in range(len(freq) - 1, 0, -1):
+            for j in freq[i]:
+                res.append(j)
                 if len(res) == k:
                     return res
+                
+        
+                
+                
+        
