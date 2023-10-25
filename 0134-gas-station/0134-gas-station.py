f02de61a428b94ebe6d9cf406c95e519bfe11e53
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-24 21:32:09 -0400

    Time: 1044 ms (86.25%), Space: 22.1 MB (66.32%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -5,12 +5,11 @@ class Solution:
         
         total = 0
         res = 0 
-        start = 0
         for i in range(len(gas)): # len(gas) = len(cost)
             diff = (gas[i] - cost[i])
             total += diff
             
             if total < 0:
                 total = 0 # reset total to 0
-                start = i + 1
-        return start
+                res = i + 1
+        return res
