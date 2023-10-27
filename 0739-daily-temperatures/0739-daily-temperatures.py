eb76695b8240da093d840f8cd93bcac1f05fe5ce
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-27 12:36:35 -0400

    Time: 1090 ms (31.72%), Space: 32.9 MB (10.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,13 +1,14 @@
 class Solution:
     def dailyTemperatures(self, temp: List[int]) -> List[int]:
-        stack = [] # pair [temp, index]
+        # Monotonic Decreasing Stack: stack in decreasing order
         res = [0] * len(temp)
+        stack = [] # pair value: [i, temp]
         
         for i, t in enumerate(temp):
-            while stack and t > stack[-1][0]:
-                stackT, stackIdx = stack.pop()
-                res[stackIdx] = (i - stackIdx)
-            stack.append([t, i])
+            # check if stack is empty and the value of the current t is > stack[-1]
+            while stack and t > stack[-1][1]:
+                stackInd, stackT = stack.pop()
+                # add the index differences to the corresponding position in res
+                res[stackInd] = (i - stackInd)
+            stack.append([i, t])
         return res
-        
-        
