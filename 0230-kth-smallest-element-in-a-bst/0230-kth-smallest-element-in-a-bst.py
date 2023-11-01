2e34f8aca0ee2cb9f89e08004fdc06faa5a21336
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-10-31 21:21:55 -0400

    Time: 62 ms (11.08%), Space: 20.3 MB (64.04%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -6,23 +6,45 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-#         res = []
-        
-#         def inOrder(node):
-#             if node is None:
-#                 return
-#             inOrder(node.left)
-#             res.append(node.val)
-#             inOrder(node.right)
-#         inOrder(root)
-#         return res[k-1]
-        stack = [root]
+        stack = [(root, False)]
+        res = []
         while stack:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            k -= 1
-            if not k:
-                return root.val
-            root = root.right
+            # while root:
+            #     stack.append(root)
+            #     root = root.left
+            # root = stack.pop()
+            # k -= 1
+            # if not k:
+            #     return root.val
+            # root = root.right
+            cur, visited = stack.pop()
+            if cur:
+                if visited:
+                    res.append(cur.val)
+                else:
+                    stack.append((cur.right, False))
+                    stack.append((cur, True))
+                    stack.append((cur.left, False))
+        return res[k - 1]
+            
+#             while root:
+#                 stack.append(root)
+#                 root = root.left
+#             root = stack.pop()
+#             k -= 1
+#             if not k:
+#                 return root.val
+#             root = root.right
+            
+#             arr = []
+            
+#             def inOrder(node):
+#                 if node is None:
+#                     return
+#                 inOrder(node.left)
+#                 arr.append(node.val)
+#                 inOrder(node.right)
+#             inOrder(root)
+#             return arr[k - 1]
+
+        
