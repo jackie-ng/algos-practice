e4676f910bbb6acbaa3e4eeedc271e5456f6a34a
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-02 14:35:33 -0400

    Time: 355 ms (16.48%), Space: 27.8 MB (6.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -7,43 +7,48 @@ class Solution:
         if not grid:
             return 0
         
-        def bfs(r, c):
-            q = collections.deque()
+        def dfs(r, c):
+            if (
+                r not in range(ROWS)
+                or c not in range(COLS)
+                or grid[r][c] == "0"
+                or (r, c) in visited
+            ):
+                return
+
             visited.add((r, c))
-            q.append((r, c))
-            
-            while q:
-                row, col = q.popleft()
-                
-                directions= [[1,0],[-1,0],[0,1],[0,-1]]
-               
-                for dr,dc in directions:
-                    r,c = row + dr, col + dc
-                    if (r in range(ROWS) and
-                        c in range(COLS) and 
-                        grid[r][c] == '1' and 
-                        (r ,c) not in visited):
-                        q.append((r , c ))
-                        visited.add((r, c ))
-                        
-#             if (r not in range(ROWS) or
-#                 c not in range(COLS) or
-#                 (r, c) in visited):
-#                 return
-            
-#             visited.add((r, c))
-#             dfs(r + 1, c)
-#             dfs(r, c + 1)
-#             dfs(r - 1, c)
-#             dfs(r, c - 1)
-            
-            
+            dfs(r + 1, c)
+            dfs(r, c + 1)
+            dfs(r - 1, c)
+            dfs(r, c - 1)
+
         for r in range(ROWS):
             for c in range(COLS):
                 if grid[r][c] == "1" and (r, c) not in visited:
                     islands += 1
-                    bfs(r, c)
-                    # dfs(r, c)
+                    dfs(r, c)
         return islands
+
+#         def bfs(r, c):
+#             q = collections.deque()
+#             visited.add((r, c))
+#             q.append((r, c))
+            
+#             while q:
+#                 row, col = q.popleft()
+                
+#                 directions= [[1,0],[-1,0],[0,1],[0,-1]]
+               
+#                 for dr,dc in directions:
+#                     r,c = row + dr, col + dc
+#                     if (r in range(ROWS) and
+#                         c in range(COLS) and 
+#                         grid[r][c] == '1' and 
+#                         (r ,c) not in visited):
+#                         q.append((r , c ))
+#                         visited.add((r, c ))
+
+
+
 
     
