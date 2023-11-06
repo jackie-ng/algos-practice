bc8a5147f93674f6aae6832429b1a3327a127028
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-06 17:13:32 -0500

    Time: 57 ms (77.62%), Space: 16.7 MB (70.64%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -4,24 +4,24 @@ class Solution:
         rank = [1] * (len(edges) + 1)
 
         def find(n):
-            p = parent[n]
-            while p != parent[p]:
-                parent[p] = parent[parent[p]] # path compression: set p parent to its grandparent to shorten the path as we go up to the path
-                p = parent[p] # while p is not equal to its own parent, keep going up the length
-            return p # once we got into the root parent => return parent[n]
+            if parent[n] != n:
+                parent[n] = find(parent[n])
+            return parent[n]
 
         # return False if already unioned
         def union(n1, n2):
-            p1, p2 = find(n1), find(n2) # find the root parent first
+            r1, r2 = find(n1), find(n2) # find the root parent first
 
-            if p1 == p2:
+            if r1 == r2:
                 return False 
-            if rank[p1] > rank[p2]: # p1 is the parent of p2
-                parent[p2] = p1
-                rank[p1] += rank[p2]
+            if rank[r1] > rank[r2]: # p1 is the parent of p2
+                parent[r2] = r1
+                # rank[p1] += rank[p2]
             else: # p2 is the parent of p1
-                parent[p1] = p2
-                rank[p2] += rank[p1]
+                parent[r1] = r2
+                rank[r2] += 1
+                # rank[p2] += rank[p1]
+                
             return True
 
         for n1, n2 in edges:
