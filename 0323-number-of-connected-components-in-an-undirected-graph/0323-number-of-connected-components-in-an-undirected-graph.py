68f5fa54abf7a7579b4d7a5c7bb57cc95be587cb
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-06 19:33:02 -0500

    Time: 99 ms (64.92%), Space: 17.7 MB (64.79%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,29 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        parent = [ i for i in range(n)]
+        root = [i for i in range(n)]
         rank = [1] * n
+        res = n
         
-        def find(n1):
-            res = n1
+        def find(n):
+            if root[n] != n:
+                root[n] = find(root[n])
+            return root[n]
             
-            while res != parent[res]:
-                parent[res] = parent[parent[res]]
-                res = parent[res]
-            return res
-        
         def union(n1, n2):
-            p1, p2 = find(n1), find(n2)
+            r1, r2 = find(n1), find(n2)
             
-            if p1 == p2:
+            if r1 == r2:
                 return 0
-            if rank[p2] > rank[p1]:
-                parent[p1] = p2
-                rank[p2] += rank[p1]
-                
+            if rank[r1] > rank[r2]:
+                root[r2] = r1
+            elif rank[r2] > rank[r1]:
+                root[r1] = r2
             else:
-                parent[p2] = p1
-                rank[p1] += rank[p2]
+                root[r1] = r2
+                rank[r2] += rank[r1]
             return 1
         
-        res = n
         for n1, n2 in edges:
             res -= union(n1, n2)
         return res
+        
