ee060d372e270a6d36f20d65f9c6fb51b4e739d5
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-13 23:52:07 -0500

    Time: 40 ms (48.89%), Space: 16.5 MB (12.31%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob1, rob2 = 0, 0
         
-        # [rob1, rob2, n, n+1,...]
-        for n in nums:
-            rob1, rob2 = rob2, max(rob1+n,rob2)
-        return rob2
+        ### Top Down ###
+        def top_down(i):
+            # no more houses left
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+            optimal_profit = max(top_down(i + 1), top_down(i + 2) + nums[i])
+            memo[i] = optimal_profit
+            return optimal_profit
+        memo = {}
+        return top_down(0)
+        
+        ### Bottom Up ###
+#         rob1, rob2 = 0, 0
+        
+#         # [rob1, rob2, n, n+1,...]
+#         for n in nums:
+#             rob1, rob2 = rob2, max(rob1+n,rob2)
+#         return rob2
