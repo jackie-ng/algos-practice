1c8f24ee9fcf7b4584db4bb0c611d645b9e2ba35
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-14 10:43:50 -0500

    Time: 40 ms (51.93%), Space: 16.3 MB (13.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        
+        # reuse house robber I function
         def house_robber(nums):
             rob1, rob2 = 0, 0
             
             for n in nums:
                 rob1, rob2 = rob2, max(rob1 + n, rob2)
             return rob2
-        
+        # nums[0] for edge case, skip nums[0], skip nums[-1]
         return max(nums[0], house_robber(nums[1:]), house_robber(nums[:-1]))
