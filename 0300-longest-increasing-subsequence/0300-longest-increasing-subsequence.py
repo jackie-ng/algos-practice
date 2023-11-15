b26ec1b49247741c124ff7f26b217422356c07e6
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-15 15:08:23 -0500

    Time: 86 ms (80.31%), Space: 16.7 MB (12.23%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -28,3 +28,16 @@ class Solution:
                 length += 1
 
         return length
+    
+# Initializes an array tails to keep track of the tails of the increasing subsequences.
+# The binary_search_insert function performs binary search to find the position where the current element can be inserted in the increasing subsequence.
+# For each element in the input array nums, the algorithm finds its position in the tails array using binary search.
+# If the position is equal to the current length, it means a new element is added, so increment the length.
+# Otherwise, update the element at that position with the current element.
+# The final result is the length of the LIS.
+# Time complexity of O(n log n), where n is the length of the input array nums. The binary search ensures efficient insertion and updating of elements in the tails array.
+
+
+
+
+
