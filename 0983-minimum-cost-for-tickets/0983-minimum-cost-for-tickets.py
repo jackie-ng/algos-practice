1f6d484daa84179522f6b881bec85d864066990b
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-14 23:19:52 -0500

    Time: 56 ms (31.42%), Space: 16.5 MB (47.25%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -12,12 +12,17 @@ class Solution:
     # 2. i not in days:
     # dp[i] = dp[i-1]
     # which simply means we don't have to spend money, and total costs remains same
-            dp=[0 for i in range(days[-1]+1)]
+            dp=[0 for i in range(days[-1] + 1)]
             dy = set(days)
             for i in range(days[-1]+1):
-                if i not in dy: dp[i]=dp[i-1]
-                else: dp[i]=min(dp[max(0,i-7)]+costs[1],dp[max(0,i-1)]+costs[0],dp[max(0,i-30)]+costs[2])
+                if i not in dy: 
+                    dp[i]=dp[i-1]
+                else: 
+                    dp[i]=min(dp[max(0,i-7)]+costs[1],
+                              dp[max(0,i-1)]+costs[0],
+                              dp[max(0,i-30)]+costs[2])
             return dp[-1]
+        
 #     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
 #         memo = {}
         
