09a62836ea97c491df68d93ed1417d4410f68887
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-15 00:37:16 -0500

    Time: 58 ms (77.06%), Space: 16.5 MB (68.19%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,18 +1,25 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-        # Memoization
-        stoneSum = sum(stones)
-        target = ceil(stoneSum / 2)
+#         # Memoization
+#         stoneSum = sum(stones)
+#         target = ceil(stoneSum / 2)
 
-        def dfs(i, total):
-            if total >= target or i == len(stones):
-                return abs(total - (stoneSum - total))
-            if (i, total) in dp:
-                return dp[(i, total)]
+#         def dfs(i, total): # total so far
+#             if total >= target or i == len(stones):
+#                 # otherPile = stoneSum - total, abs(total - (stoneSum - total)) = smash them
+#                 return abs(total - (stoneSum - total)) 
+#             if (i, total) in memo:
+#                 return memo[(i, total)]
 
-            dp[(i, total)] = min(dfs(i + 1, total),
-                                 dfs(i + 1, total + stones[i]))
-            return dp[(i, total)]
+#             memo[(i, total)] = min(dfs(i + 1, total), # not include cur val
+#                                  dfs(i + 1, total + stones[i])) # include cur val at i
+#             return memo[(i, total)]
 
-        dp = {}
-        return dfs(0, 0)
+#         memo = {}
+#         return dfs(0, 0)
+
+        # Tabulation
+        dp = {0}
+        for a in stones:
+            dp = {a + x for x in dp} | {abs(a - x) for x in dp}
+        return min(dp)
