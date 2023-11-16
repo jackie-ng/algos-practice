fe1baf44f27b29a3a7f494ef6abe963778317eb4
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-15 22:35:05 -0500

    Time: 543 ms (50.84%), Space: 16.4 MB (47.60%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        ### Expand from the middle ###
         res = ""
 
         for i in range(len(s)):
@@ -11,3 +12,38 @@ class Solution:
                     r += 1
 
         return res
+    
+        ### DP ###
+
+#         n = len(s)
+
+#         # Create a table to store whether a substring is a palindrome
+#         dp = [[False] * n for _ in range(n)]
+
+#         start = 0  # Start index of the longest palindromic substring
+#         max_length = 1  # Length of the longest palindromic substring
+
+#         # Every individual character is a palindrome
+#         for i in range(n):
+#             dp[i][i] = True
+
+#         # Check for palindromes of length 2
+#         for i in range(n - 1):
+#             if s[i] == s[i + 1]:
+#                 dp[i][i + 1] = True
+#                 start = i  # Update start index
+#                 max_length = 2  # Update length
+
+#         # Check for palindromes of length 3 or more
+#         for length in range(3, n + 1):
+#             for i in range(n - length + 1):
+#                 j = i + length - 1
+#                 # Check if the substring from i to j is a palindrome
+#                 dp[i][j] = dp[i + 1][j - 1] and s[i] == s[j]
+
+#                 if dp[i][j]:
+#                     start = i  # Update start index
+#                     max_length = length  # Update length
+
+#         # Return the longest palindromic substring
+#         return s[start:start + max_length]
