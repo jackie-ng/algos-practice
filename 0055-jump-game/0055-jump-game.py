4e79263e7b20e473b26e9baee6db75605c468c0e
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-17 14:00:48 -0500

    Time: 9914 ms (5.00%), Space: 34.3 MB (5.10%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,8 +1,33 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        goal = len(nums) - 1
+#         goal = len(nums) - 1
         
-        for i in range(len(nums) - 1, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-        return True if goal == 0 else False
+#         for i in range(len(nums) - 1, -1, -1):
+#             if i + nums[i] >= goal:
+#                 goal = i
+#         return True if goal == 0 else False
+    
+        n = len(nums)
+        memo = [None] * n
+
+        def can_reach(position):
+            # Base case: If the last index is reached, return True
+            if position == n - 1:
+                return True
+
+            # If the result for the current position is already memoized, return it
+            if memo[position] is not None:
+                return memo[position]
+
+            # Explore all possible jumps from the current position
+            max_jump = min(position + nums[position], n - 1)
+            for next_position in range(position + 1, max_jump + 1):
+                if can_reach(next_position):
+                    memo[position] = True
+                    return True
+
+            # If no jump leads to the last index, memoize and return False
+            memo[position] = False
+            return False
+
+        return can_reach(0)
