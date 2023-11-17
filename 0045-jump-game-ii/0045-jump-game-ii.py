a3ddf4bc1345f92776ae2ee49da652d6eb1fb1b7
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-17 15:23:55 -0500

    Time: 153 ms (36.15%), Space: 17.5 MB (12.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,16 +1,5 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-#         res = 0
-#         l = r = 0
-        
-#         while r < len(nums) - 1: # while r pointer has not reach the end of nums
-#             farthest = 0 # initialize farthest 
-#             for i in range(l, r + 1): # for the range of the window
-#                 farthest = max(farthest, i + nums[i]) # calculate the farthest jump can go
-#             l = r + 1 # update left pointer to the next adjacent of right pointer 
-#             r = farthest # update right pointer
-#             res += 1 # increment result after each iteration
-#         return res
                 
         n = len(nums)
         jumps = 0
