b597572e439e61a1e24a71349dae9e8ec1d83b7a
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-17 11:01:44 -0500

    Time: 791 ms (96.79%), Space: 30.6 MB (61.40%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,14 +1,19 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        t1_cost, t2_cost = float('inf'), float('inf')
-        t1_profit, t2_profit = 0, 0
-        
-        for price in prices:
-            # the maximum profit if only one transaction is allowed
-            t1_cost = min(t1_cost, price)
-            t1_profit = max(t1_profit, price - t1_cost)
-            # reinvest the gained profit in the second transaction
-            t2_cost = min(t2_cost, price - t1_profit)
-            t2_profit = max(t2_profit, price - t2_cost)
+        if not prices or len(prices) < 2:
+                return 0
 
-        return t2_profit
+        # Initialize variables to keep track of the first and second buy and sell
+        buy1, sell1, buy2, sell2 = float('inf'), 0, float('inf'), 0
+
+        for price in prices:
+            # Update the first buy and sell
+            buy1 = min(buy1, price)
+            sell1 = max(sell1, price - buy1)
+
+            # Update the second buy and sell
+            buy2 = min(buy2, price - sell1)
+            sell2 = max(sell2, price - buy2)
+
+        # Return the maximum profit after two transactions
+        return sell2
