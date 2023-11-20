8dfc22bf0354a773ea823cb1c3ae726721a1399b
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-20 14:33:04 -0500

    Time: 39 ms (65.94%), Space: 16.4 MB (27.06%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,7 +1,11 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
+        # Iterate through each bit position (32 bits in total)
         for i in range(32):
+            # Extract the i-th bit from n
             bit = (n >> i) & 1
+            # Set the corresponding bit in the result (res) by shifting it to the correct position
             res = res | (bit << (31 - i))
+        # Return the reversed integer
         return res
