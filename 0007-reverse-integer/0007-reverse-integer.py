dedb42f72034026b65557ef718352954b3508189
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-20 20:24:14 -0500

    Time: 38 ms (69.01%), Space: 16.2 MB (34.99%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -7,14 +7,18 @@ class Solution:
         MAX = 2147483647  #  2^31 - 1
 
         res = 0
+        
+        # continues until the input integer x becomes zero
         while x:
-            digit = int(math.fmod(x, 10))  # (python dumb) -1 %  10 = 9
+            lastDigit = int(math.fmod(x, 10))  # (python dumb) -1 %  10 = 9
+            # Update x by removing its last digit
             x = int(x / 10)  # (python dumb) -1 // 10 = -1
 
-            if res > MAX // 10 or (res == MAX // 10 and digit > MAX % 10):
+            # Check if adding the current digit to the reversed result would cause an overflow or underflow
+            if res > MAX // 10 or (res == MAX // 10 and lastDigit > MAX % 10):
                 return 0
-            if res < MIN // 10 or (res == MIN // 10 and digit < MIN % 10):
+            if res < MIN // 10 or (res == MIN // 10 and lastDigit < MIN % 10):
                 return 0
-            res = (res * 10) + digit
+            res = (res * 10) + lastDigit
 
         return res
