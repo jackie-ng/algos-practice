9b4d8c964fa5336449a8852190350b763ed10d7e
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-20 20:29:00 -0500

    Time: 41 ms (30.99%), Space: 16.4 MB (7.19%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -8,14 +8,17 @@ class Solution:
         # abs(a) >= abs(b) --> 
         # a determines the sign
         sign = 1 if a > 0 else -1
-        if a * b >= 0:
-            # sum of two positive integers x + y
+        
+        if a * b >= 0: # both a and b have the same sign
             # where x > y
+            # Update x to be the XOR of x and y, and update y to be the left shift of the bitwise AND of x and y. 
+            # This simulates the addition of x and y without carrying. (x + y)
             while y:
                 x, y = x ^ y, (x & y) << 1
-        else:
-            # difference of two integers x - y
+        else: # a and b have different sign
             # where x > y
+            # Update x to be the XOR of x and y, and update y to be the left shift of the bitwise AND of the bitwise negation of x and y. 
+            # This simulates the subtraction of y from x (x - y)
             while y:
                 x, y = x ^ y, ((~x) & y) << 1
         
