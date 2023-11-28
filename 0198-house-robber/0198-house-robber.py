f2c80289710123084d57eb427002df3a2f5d0bea
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-28 12:31:30 -0500

    Time: 37 ms (73.60%), Space: 16.3 MB (17.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,19 +1,16 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        ### Top Down ###
-        def top_down(i):
-            # no more houses left
-            if i >= len(nums):
-                return 0
-            if i in memo:
-                return memo[i]
-            optimal_profit = max(top_down(i + 1), top_down(i + 2) + nums[i])
-            memo[i] = optimal_profit
-            return optimal_profit
-        memo = {}
-        return top_down(0)
-        
+        # ### Top Down ###
+        # def top_down(i):
+        #     if i >= len(nums):
+        #         return 0
+        #     if i in memo:
+        #         return memo[i]
+        #     memo[i] = max(top_down(i + 1), top_down(i + 2) + nums[i]) # optimal_profit
+        #     return memo[i]
+        # memo = {}
+        # return top_down(0)
         ### Bottom Up ###
 #         rob1, rob2 = 0, 0
         
@@ -21,3 +18,15 @@ class Solution:
 #         for n in nums:
 #             rob1, rob2 = rob2, max(rob1+n,rob2)
 #         return rob2
+
+        if len(nums) == 1:
+            return nums[0]
+        
+        dp = [None] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        
+        for i in range(2,len(nums)):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+        
+        return max(dp)
