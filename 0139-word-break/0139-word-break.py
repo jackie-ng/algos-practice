aab08648f7b0dbcfe587c777d90f0f69289110c3
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-28 16:14:14 -0500

    Time: 37 ms (91.52%), Space: 16.4 MB (66.22%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,42 +1,35 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False] * len(s)
+        for i in range(len(s)):
+            for word in wordDict:
+                # Handle out of bounds case
+                if i < len(word) - 1:
+                    continue
 
-#         memo = {}
-
-#         def canBreak(start):
-#             # If the start index reaches the end of the string, it can be segmented
-#             if start == len(s):
-#                 return True
-
-#             # If the result for the current start index is already memoized, return it
-#             if start in memo:
-#                 return memo[start]
-
-#             # Iterate through the string from the current start index
-#             for end in range(start + 1, len(s) + 1):
-#                 # Check if the substring from start to end is in the word dictionary
-#                 if s[start:end] in wordDict and canBreak(end):
-#                     # Memoize the result and return True
-#                     memo[start] = True
-#                     return True
-
-#             # If no valid segmentation is found, memoize the result and return False
-#             memo[start] = False
-#             return False
-
-#         # Start the recursion from the beginning of the string
-#         return canBreak(0)        
+                if i == len(word) - 1 or dp[i - len(word)]:
+                    if s[i - len(word) + 1:i + 1] == word:
+                        dp[i] = True
+                        break
+        return dp[-1]
     
-            dp = [False] * len(s)
-            for i in range(len(s)):
-                for word in wordDict:
-                    # Handle out of bounds case
-                    if i < len(word) - 1:
-                        continue
-
-                    if i == len(word) - 1 or dp[i - len(word)]:
-                        if s[i - len(word) + 1:i + 1] == word:
-                            dp[i] = True
-                            break
-
-            return dp[-1]
+#         memo = {}
+#         def topDown(i):
+#             # Base case: if the index can reach the end of the string, it can be segmented
+#             if i == len(s):
+#                 return True
+#             # if the result for the current index is already memoized, return
+#             if i in memo:
+#                 return memo[i]
+#             # Recursive case: Iterate through s. i: start, j: end
+#             for j in range(i + 1, len(s) + 1):
+#                 if s[i:j] in wordDict and topDown(j):
+#                     memo[i] = True
+#                     return True
+#             memo[i] = False
+#             return False
+#         return topDown(0)
+  
+    
+    
+            
