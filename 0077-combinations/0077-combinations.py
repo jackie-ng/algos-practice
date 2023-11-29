d7518d14f0a80adef3a5c53eafe0f38d26ae15aa
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-28 20:02:18 -0500

    Time: 933 ms (38.76%), Space: 65.1 MB (22.51%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,16 +1,13 @@
 class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:   
-        res = []
-        
+    def combine(self, n: int, k: int) -> List[List[int]]:       
+        res  = []
         def backtrack(start, subset):
             if len(subset) == k:
                 res.append(subset[:])
                 return
-            
-            # decision/recursive case
-            for num in range(start, n + 1):
+            for num in range(start, n+1):
                 subset.append(num)
-                backtrack(num + 1, subset)
+                backtrack(num+1, subset)
                 subset.pop()
         backtrack(1, [])
         return res
