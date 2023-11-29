1d8812e267ccdcea79d43efede5e568131e2cf49
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-28 20:11:21 -0500

    Time: 72 ms (64.02%), Space: 16.3 MB (59.59%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,26 +1,46 @@
 class Solution:
-    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-
+    def combinationSum2(self, nums: List[int], target: int) -> List[List[int]]:
+#         candidates.sort()
+#         res = []
+#         subset = []
+        
+#         # decrease the target everytime we traverse
+#         def backtrack(pos, target):
+#             if target == 0:
+#                 res.append(subset[:])
+#                 return
+#             if target <= 0:
+#                 return
+#             prev = -1
+#             for i in range(pos, len(candidates)):
+#                 # if current candidate is equal to the previous candidate =>  skip
+#                 if candidates[i] == prev:
+#                     continue
+#                 subset.append(candidates[i])
+#                 backtrack(i + 1, target - candidates[i])
+#                 subset.pop()
+#                 prev = candidates[i]
+        
+#         backtrack(0, target)
+#         return res
+        nums.sort()
         res = []
         subset = []
-        # decrease the target everytime we traverse
-        def backtrack(pos, target):
-            if target == 0:
-                res.append(subset.copy())
+        
+        def backtrack(pos, sum):
+            if sum == target:
+                res.append(subset[:])
                 return
-            if target <= 0:
+            if sum > target:
                 return
-
             prev = -1
-            for i in range(pos, len(candidates)):
-                # if current candidate is equal to the previous candidate =>  skip
-                if candidates[i] == prev:
+            for i in range(pos, len(nums)):
+                if nums[i] == prev:
                     continue
-                subset.append(candidates[i])
-                backtrack(i + 1, target - candidates[i])
+                subset.append(nums[i])
+                backtrack(i+1, sum+nums[i])
                 subset.pop()
-                prev = candidates[i]
-
-        backtrack(0, target)
+                prev = nums[i]
+        backtrack(0, 0)
         return res
+        
