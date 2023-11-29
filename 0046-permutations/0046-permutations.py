3847e1cde6aa8e1795660457fc7d15197fa3c2f2
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-28 20:44:14 -0500

    Time: 37 ms (93.73%), Space: 16.5 MB (20.26%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,19 +1,23 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
+        res = []
         
         # base case
-        if (len(nums)) == 1:
+        if len(nums) == 1:
             return [nums[:]]
-        
+        # recursive case
         for i in range(len(nums)):
             n = nums.pop(0)
-            perms = self.permute(nums)
-            
-    
-            for perm in perms:
-                perm.append(n)
-            result.extend(perms)
+            subsets = self.permute(nums)
+            for subset in subsets:
+                subset.append(n)
+            res.extend(subsets)
+            # clean up
             nums.append(n)
-        
-        return result
+        return res
+
+# - The code uses a for loop to iterate over each element of the list `nums`.
+# - Inside the loop, it pops an element (`n`) from the list and recursively generates all permutations of the remaining elements (`subsets`).
+# - For each subset in the permutations, it appends the popped element `n` to create a new permutation.
+# - These new permutations are added to the result list (`res`) using the `extend` method.
+# - After the loop, the popped element n is appended back to the list nums. This step is necessary to restore the original state of the list before the next iteration.
