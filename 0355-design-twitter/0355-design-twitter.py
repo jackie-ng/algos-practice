94fd310daf83514076fb3ad62563563db88ddbc5
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-29 16:26:38 -0500

    Time: 44 ms (17.84%), Space: 16.6 MB (14.28%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -9,16 +9,33 @@ class Twitter:
         self.count -= 1
 
     def getNewsFeed(self, userId: int) -> List[int]:
+        # Retrieve the 10 most recent tweet ids in the user's news feed. 
+        # Each item in the news feed must be posted by users who the user followed or by the user herself. 
+        # Tweets must be ordered from most recent to least recent.
+        
+        # 1. res will store the result, i.e., the list of tweet IDs in the user's news feed. 
+        # minHeap is a min-heap that will be used to efficiently retrieve the most recent tweets.
         res = []
         minHeap = []
-
+        
+        # 2. Adding the user to their own followees
+        # This ensures that the user's own tweets will be included in their news feed.
         self.followMap[userId].add(userId)
-        for followeeId in self.followMap[userId]:
-            if followeeId in self.tweetMap:
-                index = len(self.tweetMap[followeeId]) - 1
-                count, tweetId = self.tweetMap[followeeId][index]
-                heapq.heappush(minHeap, [count, tweetId, followeeId, index - 1])
+        
+        # 3. Populating the min heap with initial tweets
+        # It iterates through the users that the specified user is following (followee) 
+        # and pushes their latest tweets onto the min heap. 
+        # Each element in the heap is a list [count, tweetId, followeeId, index], 
+        # count: timestamp, tweetId: ID of the tweet, followeeId: ID of the user who posted the tweet, 
+        # index: index of the tweet in the user's tweet list.
+        for followee in self.followMap[userId]:
+            if followee in self.tweetMap:
+                index = len(self.tweetMap[followee]) - 1
+                count, tweetId = self.tweetMap[followee][index]
+                heapq.heappush(minHeap, [count, tweetId, followee, index - 1])
 
+        # 4. Retrieving the 10 most recent tweets from the min heap
+        # It pops elements from the min heap (which are the tweets with the smallest timestamps) and appends the tweet ID to the result list. If there are more tweets from the same user, they are pushed onto the heap for further consideration.
         while minHeap and len(res) < 10:
             count, tweetId, followeeId, index = heapq.heappop(minHeap)
             res.append(tweetId)
