b359fddf87ff1543aa3b3b4968d8db03ee245ac7
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-29 22:48:34 -0500

    Time: 395 ms (87.87%), Space: 17 MB (5.03%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -1,17 +1,47 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        # frequencies of the tasks
-        frequencies = [0] * 26
-        for t in tasks:
-            frequencies[ord(t) - ord('A')] += 1
-        frequencies.sort()
-        
-         # max frequency
-        f_max = frequencies.pop()
-        idle_time = (f_max - 1) * n
-        
-        while frequencies and idle_time > 0:
-            idle_time -= min(f_max - 1, frequencies.pop())
-        idle_time = max(0, idle_time)
+        task_count = [0] * 26  # Assuming uppercase English letters
+        for task in tasks:
+            task_count[ord(task) - ord('A')] += 1
 
-        return idle_time + len(tasks)
+        max_count = max(task_count)
+        max_count_tasks = task_count.count(max_count)
+
+        # Calculate the total units needed
+        total_units = (max_count - 1) * (n + 1) + max_count_tasks
+
+        return max(total_units, len(tasks))
+    
+#     ##SOLVING THIS USING HEAP##
+    
+#         # Count the frequency of each task
+#         count = Counter(tasks)
+        
+#         # Create a max heap with negative frequencies
+#         maxHeap = [-cnt for cnt in count.values()]
+#         heapq.heapify(maxHeap)
+
+#         # Initialize time and a deque to store pairs of [-cnt, idleTime]
+#         time = 0
+#         q = deque()
+
+#         # Continue until both the maxHeap and the deque are empty
+#         while maxHeap or q:
+#             time += 1
+
+#             # If maxHeap is empty, there are no tasks left to schedule
+#             if not maxHeap:
+#                 time = q[0][1]
+#             else:
+#                 # Schedule a task and update the maxHeap
+#                 cnt = 1 + heapq.heappop(maxHeap)
+#                 if cnt:
+#                     # Add the task and its idle time to the deque
+#                     q.append([cnt, time + n])
+
+#             # If there are tasks in the deque ready to be scheduled, update the maxHeap
+#             if q and q[0][1] == time:
+#                 heapq.heappush(maxHeap, q.popleft()[0])
+
+#         # The final value of 'time' represents the least number of units of time
+#         return time
