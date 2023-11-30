d92acb99e81d9db02a0afc2b4c283af1916dd2f0
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-30 18:38:41 -0500

    Time: 1148 ms (66.87%), Space: 55.2 MB (53.06%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -5,9 +5,13 @@ class Solution:
         res = 0
         prevEnd = intervals[0][1]
         
-        for start, end in intervals[1:]:
+        for start, end in intervals[1:]: # check the remaining intervals
+            # if start of current interval >= end of previous interval
+            # => not overlapping
             if start >= prevEnd: # not overlapping
                 prevEnd = end # update the prevEnd to the newEnd
+            # else, there is an overlap => increase res by 1
+            # => update previous end to the minimum between the previous and the current interval
             else:
                 res += 1
                 prevEnd = min(end, prevEnd) # update prevEnd
