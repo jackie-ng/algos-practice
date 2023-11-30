1d0126067cdb042408743c222fce1dad2fb94005
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-30 14:20:47 -0500

    Time: 84 ms (29.08%), Space: 19.8 MB (13.50%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -3,14 +3,13 @@ class Solution:
     @param intervals: an array of meeting time intervals
     @return: if a person could attend all meetings
     """
-
     def canAttendMeetings(self, intervals):
-        intervals.sort(key=lambda i: i[0])
-
+        intervals.sort(key=lambda i:i[0]) # sort based on the start meeting time
+        # loop through intervals array to check every single meeting
         for i in range(1, len(intervals)):
-            i1 = intervals[i - 1]
-            i2 = intervals[i]
-
-            if i1[1] > i2[0]:
+            i1 = intervals[i-1] # previous meeting
+            i2 = intervals[i] # current meeting
+            # if the end time of previous meeting > the start time of current meeting => False
+            if i1[1] > i2[0]: 
                 return False
         return True
