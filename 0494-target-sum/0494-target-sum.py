78557f2728c396bb474908c614b329dea96cbb14
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-01 14:47:23 -0500

    Time: 235 ms (65.89%), Space: 16.6 MB (72.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,18 @@
 class Solution:
-    def findTargetSumWays(self, nums: List[int], S: int) -> int:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        
+#         memo = {}  # (index, total) -> # of ways
+
+#         def backtrack(i, total):
+#             if i == len(nums):
+#                 return 1 if total == target else 0
+#             if (i, total) in memo:
+#                 return memo[(i, total)]
+#             # add the value at index i + subtract the value at index i
+#             memo[(i, total)] = backtrack(i + 1, total + nums[i]) + backtrack(i + 1, total - nums[i])    
+#             return memo[(i, total)]
+
+#         return backtrack(0, 0)
         
         # dp
         # Two variables: index + sum
@@ -9,10 +22,11 @@ class Solution:
         lookup = collections.Counter()
         lookup[nums[0]] += 1
         lookup[-nums[0]] += 1
-        
+        print("lookup", lookup)
         for i in range(1, len(nums)):
             # parepare a new map for the next iteration
             nextLookup = collections.Counter()
+            print("nextLookup", nextLookup)
             # for each iteration, update each possible sum and it's count 
             for s in lookup:
                 # current to be possitive
@@ -21,5 +35,4 @@ class Solution:
                 nextLookup[s - nums[i]] += lookup[s]
             # assign the new map to be the current map
             lookup = nextLookup
-            
-        return lookup[S]
+        return lookup[target]
