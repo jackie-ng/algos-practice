19bf539e1f380416d6dee696c938e52098b9692d
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-01 10:56:16 -0500

    Time: 86 ms (23.09%), Space: 22.5 MB (21.29%) - LeetHub

--- 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.py
@@ -1,30 +1,32 @@
 class Solution:
     def lastStoneWeightII(self, stones: List[int]) -> int:
-#         # Memoization
-#         stoneSum = sum(stones)
-#         target = ceil(stoneSum / 2)
+        # Memoization
+        stoneSum = sum(stones)
+        target = ceil(stoneSum / 2)
 
-#         def dfs(i, total): # total so far
-#             if total >= target or i == len(stones):
-#                 # otherPile = stoneSum - total, abs(total - (stoneSum - total)) = smash them
-#                 return abs(total - (stoneSum - total)) 
-#             if (i, total) in memo:
-#                 return memo[(i, total)]
+        def dfs(i, total): # total so far
+            if total >= target or i == len(stones):
+                # otherPile = stoneSum - total, abs(total - (stoneSum - total)) = smash them
+                return abs(total - (stoneSum - total)) 
+            if (i, total) in memo:
+                return memo[(i, total)]
 
-#             memo[(i, total)] = min(dfs(i + 1, total), # not include cur val
-#                                  dfs(i + 1, total + stones[i])) # include cur val at i
-#             return memo[(i, total)]
+            memo[(i, total)] = min(dfs(i + 1, total), # not include cur val
+                                 dfs(i + 1, total + stones[i])) # include cur val at i
+            return memo[(i, total)]
 
-#         memo = {}
-#         return dfs(0, 0)
+        memo = {}
+        return dfs(0, 0)
+    
+    
 
         # Tabulation
     
-        memo = {0}
-        sumA = sum(stones)
-        for a in stones:
-            memo |= {a + i for i in memo}
-        return min(abs(sumA - i - i) for i in memo)
+        # memo = {0}
+        # sumA = sum(stones)
+        # for a in stones:
+        #     memo |= {a + i for i in memo}
+        # return min(abs(sumA - i - i) for i in memo)
     
 #         dp = {0}
 #         for a in stones:
