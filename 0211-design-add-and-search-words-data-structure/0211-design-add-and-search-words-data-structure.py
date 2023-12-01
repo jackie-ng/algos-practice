394dfcd8a595bccf6df86097edebdfe91e2fd44d
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-11-30 21:44:44 -0500

    Time: 2157 ms (68.48%), Space: 80.4 MB (31.12%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,10 +1,10 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}  # a : TrieNode
-        self.word = False
-
-
+        self.children = {}
+        self.endOfWord = False
+        
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
 
@@ -14,23 +14,34 @@ class WordDictionary:
             if c not in cur.children:
                 cur.children[c] = TrieNode()
             cur = cur.children[c]
-        cur.word = True
+        cur.endOfWord = True
 
     def search(self, word: str) -> bool:
         def dfs(j, root):
             cur = root
-
+            
             for i in range(j, len(word)):
+                # c: current charater in word
                 c = word[i]
+                # If c is ".", it means it can match any character. 
+                # So, the code recursively explores all possible children nodes for each character in the Trie.
                 if c == ".":
                     for child in cur.children.values():
-                        if dfs(i + 1, child):
+                        if dfs(i + 1, child): # reach the end of the word
                             return True
                     return False
+                # checks if the character is in the current Trie node's children. 
+                # If not, it means the word doesn't exist in the Trie, and the function returns False.
                 else:
                     if c not in cur.children:
                         return False
                     cur = cur.children[c]
-            return cur.word
+            return cur.endOfWord
 
         return dfs(0, self.root)
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
