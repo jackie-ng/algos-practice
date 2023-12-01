30c7c90b52fae5b65d79539255b75191dfdad764
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-01 16:14:19 -0500

    Time: 263 ms (57.08%), Space: 16.7 MB (60.15%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -19,31 +19,36 @@ class Solution:
 
 #         return dfs(0, 0)
 
+        # # DYNAMIC PROGRAMMING
+        # # Time: O(n*m)
+        # # Memory: O(n*m)
+        # dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
+        # dp[0] = [1] * (len(coins) + 1)
+        # for a in range(1, amount + 1):
+        #     for i in range(len(coins) - 1, -1, -1):
+        #         ## update its value by copying the value from the cell to the right
+        #         dp[a][i] = dp[a][i + 1]
+        #         ## current coin can be included without exceeding the amount. 
+        #         ## add the value from the cell above and to the left (dp[a - coins[i]][i]) to the current cell.
+        #         if a - coins[i] >= 0:
+        #             remaining = a - coins[i]
+        #             dp[a][i] += dp[remaining][i]
+        # return dp[amount][0]
+
         # DYNAMIC PROGRAMMING
         # Time: O(n*m)
-        # Memory: O(n*m)
-        dp = [[0] * (len(coins) + 1) for i in range(amount + 1)]
-        dp[0] = [1] * (len(coins) + 1)
-        for a in range(1, amount + 1):
-            for i in range(len(coins) - 1, -1, -1):
-                dp[a][i] = dp[a][i + 1]
+        # Memory: O(n) where n = amount
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = [0] * (amount + 1)
+            nextDP[0] = 1
+
+            for a in range(1, amount + 1):
+                # not including any coin by copying the coin over from the previous table dp to nextDP
+                nextDP[a] = dp[a]
+                # including the current coin
                 if a - coins[i] >= 0:
-                    remaining = a - coins[i]
-                    dp[a][i] += dp[remaining][i]
-        return dp[amount][0]
-
-#         # DYNAMIC PROGRAMMING
-#         # Time: O(n*m)
-#         # Memory: O(n) where n = amount
-#         dp = [0] * (amount + 1)
-#         dp[0] = 1
-#         for i in range(len(coins) - 1, -1, -1):
-#             nextDP = [0] * (amount + 1)
-#             nextDP[0] = 1
-
-#             for a in range(1, amount + 1):
-#                 nextDP[a] = dp[a]
-#                 if a - coins[i] >= 0:
-#                     nextDP[a] += nextDP[a - coins[i]]
-#             dp = nextDP
-#         return dp[amount]
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
