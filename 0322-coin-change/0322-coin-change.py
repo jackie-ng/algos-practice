1e63298b4c043057c4147154b84230ee3b6721ca
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-01 16:49:05 -0500

    Time: 1090 ms (33.99%), Space: 41.2 MB (12.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,11 +1,35 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
+        ### Top Down ###
+    
+        memo = {}
         
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0: # if the amount is non-negative
-                    dp[a] = min(dp[a], 1 + dp[a-c])
+        def dfs(amount):
+            if amount in memo: 
+                return memo[amount]
+            if amount == 0: 
+                return 0
+            if amount < 0: 
+                return float("inf")
+            memo[amount] = min([1 + dfs(amount - coin) for coin in coins])
+            return memo[amount]
+        
+        res = dfs(amount)
+        return res if res < float("inf") else -1
+        
+                
+        
+#         ### Bottom Up ###
+       
+#         # Values in this array equal the number of coins needed to achieve the cost of the index
+#         dp = [amount + 1] * (amount + 1)
+#         dp[0] = 0
+#         # Loop through every needed amount
+#         for a in range(1, amount + 1):
+#             # Loop through every needed coin value
+#             for c in coins:
+#                 # check if the coin is not bigger than the current amount
+#                 if a - c >= 0: # if the amount is non-negative
+#                     dp[a] = min(dp[a], 1 + dp[a-c]) ## coins needed to make the amount before adding the current coin to it (+1 to add the cur coin) 
                     
-        return dp[amount] if dp[amount] != amount+1 else -1
+#         return dp[amount] if dp[amount] != amount+1 else -1
