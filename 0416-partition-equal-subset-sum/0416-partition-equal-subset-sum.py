70600c078be44d09cadf55e15d80cce05955382f
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-01 19:32:07 -0500

    Time: 2203 ms (24.01%), Space: 492.8 MB (5.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,16 +1,43 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if sum(nums) % 2:
-            return False
-        dp = set() # for unique values
-        dp.add(0)
-        target = sum(nums) // 2
+#         if sum(nums) % 2:
+#             return False
+#         dp = set() # for unique values
+#         dp.add(0)
+#         target = sum(nums) // 2
         
-        for i in range(len(nums) -1, -1, -1):
-            nextDP = set()
-            for t in dp:
-                nextDP.add(t + nums[i])
-                nextDP.add(t) # save the previous values
-            dp = nextDP # reassign dp to the next DP set
-            # print(dp)
-        return True if target in dp else False
+#         for i in range(len(nums) -1, -1, -1):
+#             nextDP = set()
+#             for t in dp:
+#                 nextDP.add(t + nums[i])
+#                 nextDP.add(t) # save the previous values
+#             dp = nextDP # reassign dp to the next DP set
+#         return True if target in dp else False
+    
+
+            if not nums:
+                return True
+            n = len(nums)
+
+            # If the total sum is odd, it cannot be divided into two equal subsets.
+            if sum(nums) % 2 != 0:
+                return False
+            target = sum(nums)//2
+            memo = {}
+
+            def helper(total, i):
+                # nonlocal nums, memo
+                # Check if the state (index, current_sum) has already been visited.
+
+                if (total, i) in memo:
+                    return memo[(total, i)]
+                if i == len(nums):
+                    return False
+                if total == 0:
+                    return True
+                # Recursive case: Try including and excluding the current element.
+
+                memo[(total, i)] = helper(total-nums[i], i+1) or helper(total, i+1)
+                return memo[(total, i)]
+
+            return helper(target, 0) 
