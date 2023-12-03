b567ac6b259d4de0f285fb7791d220a2ef7f3c1d
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-03 14:24:17 -0500

    Time: 673 ms (46.12%), Space: 42.5 MB (25.55%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -29,29 +29,40 @@ class Solution:
 #         return dp[-1][-1]
     
     
-        #### We can save a lot of space by instead of keeping track of an entire 2D array, 
-        #### only keeping track of the last two columns.
+#         #### We can save a lot of space by instead of keeping track of an entire 2D array, 
+#         #### only keeping track of the last two columns.
         
-        # Make sure text1 is shorter than text 2
-        if len(text2) < len(text1):
-            text1, text2 = text2, text1
+#         # Make sure text1 is shorter than text 2
+#         if len(text2) < len(text1):
+#             text1, text2 = text2, text1
         
         
-        # The previous column starts with all 0's and like before is 1
-        # more than the length of the first word.
-        previous = [0] * (len(text1) + 1)
+#         # The previous column starts with all 0's and like before is 1
+#         # more than the length of the first word.
+#         previous = [0] * (len(text1) + 1)
         
-        # Iterate up each column, starting from the last one.
-        for col in reversed(range(len(text2))):
-            # Create a new array to represent the current column.
-            current = [0] * (len(text1) + 1)
-            for row in reversed(range(len(text1))):
-                if text2[col] == text1[row]:
-                    current[row] = 1 + previous[row + 1]
+#         # Iterate up each column, starting from the last one.
+#         for col in reversed(range(len(text2))):
+#             # Create a new array to represent the current column.
+#             current = [0] * (len(text1) + 1)
+#             for row in reversed(range(len(text1))):
+#                 if text2[col] == text1[row]:
+#                     current[row] = 1 + previous[row + 1]
+#                 else:
+#                     current[row] = max(previous[row], current[row + 1])
+#             # The current column becomes the previous one.
+#             previous = current
+        
+#         # The original problem's answer is in previous[0]. Return it.
+#         return previous[0]
+
+        ROWS, COLS = len(text1) + 1, len(text2) + 1
+        dp = [[0 for j in range(COLS)] for i in range(ROWS)]
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    dp[i+1][j+1] = 1 + dp[i][j]
                 else:
-                    current[row] = max(previous[row], current[row + 1])
-            # The current column becomes the previous one.
-            previous = current
-        
-        # The original problem's answer is in previous[0]. Return it.
-        return previous[0]
+                    dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+                    
+        return dp[-1][-1]
