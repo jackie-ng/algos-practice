2b956e1425ced98d04427bf1f50144cbadc3af43
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-03 18:01:13 -0500

    Time: 51 ms (42.37%), Space: 18 MB (7.69%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,54 +1,83 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        len_s1, len_s2, len_s3 = len(s1), len(s2), len(s3)
-        # If the lengths of s1 and s2 don't add up to the length of s3, it's impossible to form s3
-        if len_s1 + len_s2 != len_s3:
-            return False        
-        # Initialize a 2D table to store intermediate results
-        dp = [[False] * (len_s2 + 1) for _ in range(len_s1 + 1)]
+#         len_s1, len_s2, len_s3 = len(s1), len(s2), len(s3)
+#         # If the lengths of s1 and s2 don't add up to the length of s3, it's impossible to form s3
+#         if len_s1 + len_s2 != len_s3:
+#             return False        
+#         # Initialize a 2D table to store intermediate results
+#         dp = [[False] * (len_s2 + 1) for _ in range(len_s1 + 1)]
         
-        # Base case: empty strings can always interleave to form an empty string
-        dp[0][0] = True
+#         # Base case: empty strings can always interleave to form an empty string
+#         dp[0][0] = True
     
-        # Fill the first row (s1) and the first column (s2)
-        for i in range(1, len_s1 + 1):
-            dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
+#         # Fill the first row (s1) and the first column (s2)
+#         for i in range(1, len_s1 + 1):
+#             dp[i][0] = dp[i - 1][0] and s1[i - 1] == s3[i - 1]
 
-        for j in range(1, len_s2 + 1):
-            dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
+#         for j in range(1, len_s2 + 1):
+#             dp[0][j] = dp[0][j - 1] and s2[j - 1] == s3[j - 1]
             
-        # Fill the rest of the table
-        for i in range(1, len_s1 + 1):
-            for j in range(1, len_s2 + 1):
-                # Check if s1 contributes to the current position
-                dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
-                           (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]) # Check if s2 contributes to the current position
+#         # Fill the rest of the table
+#         for i in range(1, len_s1 + 1):
+#             for j in range(1, len_s2 + 1):
+#                 # Check if s1 contributes to the current position
+#                 dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or \
+#                            (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]) # Check if s2 contributes to the current position
 
-        # The result is found at the bottom-right corner of the table
-        return dp[len_s1][len_s2]
+#         # The result is found at the bottom-right corner of the table
+#         return dp[len_s1][len_s2]
 
 #         # Memoization dictionary to store results of subproblems
 #         memo = {}
 
-#         def recursiveInterleave(i, j, k):
-#             # Check if the result is already memoized
-#             if (i, j, k) in memo:
-#                 return memo[(i, j, k)]
-
+#         def dfs(i, j):
 #             # Base case: If we have reached the end of all strings
-#             if i == len(s1) and j == len(s2) and k == len(s3):
+#             if i == len(s1) and j == len(s2):
 #                 return True
+#             # Check if the result is already memoized
+#             if (i, j) in memo:
+#                 return memo[(i, j)]
+
 
 #             # Check if interleaving is possible and recurse accordingly
-#             result = (
-#                 (i < len(s1) and s1[i] == s3[k] and recursiveInterleave(i + 1, j, k + 1)) or
-#                 (j < len(s2) and s2[j] == s3[k] and recursiveInterleave(i, j + 1, k + 1))
-#             )
+#             if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+#                 return True
+#             if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+#                 return True
 
 #             # Memoize the result before returning
-#             memo[(i, j, k)] = result
-#             return result
+#             memo[(i, j)] = False
+#             return False
 
 #         # Start the recursive process with initial indices (0, 0, 0)
-#         return recursiveInterleave(0, 0, 0)
+#         return dfs(0, 0)
+    
+        memo = {}
+        if len(s1) == 0:
+            return s2 == s3
+        if len(s2) == 0:
+            return s1 == s3
+        if len(s1) + len(s2) != len(s3): 
+            return False
+        def dfs(i,j):
+            if (i,j) in memo: 
+                return memo[(i,j)]
+            
+            c1 = c2 = False 
+            
+            if i == len(s1) and j == len(s2): 
+                return True
+                
+           
+            if i<len(s1) and s1[i]==s3[i+j]:
+                c1 = dfs(i+1,j)
+           
+            if j < len(s2) and s2[j] == s3[i+j]:
+                c2 = dfs(i,j+1)  
+            
+            memo[(i,j)] = c1 or c2
+            return c1 or c2
+        
+        dfs(0,0)
+        return memo[(0,0)]
         
