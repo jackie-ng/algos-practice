77ebc91a31261fec8232c2e9f2212b507e270b82
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-04 12:15:52 -0500

    Time: 10495 ms (5.03%), Space: 40.4 MB (21.86%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,25 +1,25 @@
 class Solution:
-    def maxCoins(self, A: List[int]) -> int:
-        # dp = {}
-        # nums = [1] + nums + [1]
-        # def dfs(l, r):
-        #     if l>r:
-        #         return 0
-        #     if (l,r) in dp:
-        #         return dp[(l,r)]
-        #     dp[(l,r)] = 0
-        #     for i in range(l, r+1):
-        #         coins = nums[l-1] * nums[i] * nums[r+1]
-        #         coins += dfs(l, i-1) + dfs(i+1, r)
-        #         dp[(l,r)] = max(dp[(l,r)], coins)
-        #     return dp[(l,r)]
-        # return dfs(1, len(nums)-2)
+    def maxCoins(self, nums: List[int]) -> int:
+        memo = {}
+        nums = [1] + nums + [1]
+        def dfs(l, r):
+            if l>r:
+                return 0
+            if (l,r) in memo:
+                return memo[(l,r)]
+            memo[(l,r)] = 0
+            for i in range(l, r+1):
+                coins = nums[l-1] * nums[i] * nums[r+1]
+                coins += dfs(l, i-1) + dfs(i+1, r)
+                memo[(l,r)] = max(memo[(l,r)], coins)
+            return memo[(l,r)]
+        return dfs(1, len(nums)-2)
     
-        A, n = [1] + A + [1], len(A) + 2
-        dp = [[0] * n for _ in range(n)]
+#         A, n = [1] + A + [1], len(A) + 2
+#         dp = [[0] * n for _ in range(n)]
         
-        for i in range(n - 2, -1, -1):
-            for j in range(i + 2, n):
-                dp[i][j] = max(A[i]*A[k]*A[j] + dp[i][k] + dp[k][j] for k in range(i + 1, j))
+#         for i in range(n - 2, -1, -1):
+#             for j in range(i + 2, n):
+#                 dp[i][j] = max(A[i]*A[k]*A[j] + dp[i][k] + dp[k][j] for k in range(i + 1, j))
         
-        return dp[0][n-1]
+#         return dp[0][n-1]
