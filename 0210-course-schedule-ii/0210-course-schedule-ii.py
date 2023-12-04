2e98064ff31b0d0b5d456cbbb84f6c3100e2b057
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-04 15:48:47 -0500

    Time: 83 ms (99.48%), Space: 17.8 MB (86.19%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -8,31 +8,61 @@ class Solution:
         :rtype: List[int]
         """
 
-        adj = [[] for i in range(numCourses)]
-        visited = [0] * numCourses
-        indeg = [0] * numCourses
-        res = []
+#         preMap = { i:[] for i in range(numCourses)}
         
-        for crs, pre in prerequisites:
-            adj[pre].append(crs)
+#         for crs, pre in prerequisites:
+#             preMap[crs].append(pre)
+#         # a course has 3 possible states:
+#         # unvisited -> crs not added to output or cycle
+#         # visiting -> crs no added to output, but added to cycle
+#         # visited -> crs has been added to output
+#         res = []
+#         visit, cycle = set(), set()
+        
+#         def dfs(crs):
+#             if crs in cycle:
+#                 return False
+#             if crs in visit:
+#                 return True
             
-        q = deque()
-        
+#             cycle.add(crs)
+            
+#             for pre in preMap[crs]:
+#                 if not dfs(pre):
+#                     return False
+#             cycle.remove(crs)
+#             visit.add(crs)
+#             res.append(crs)
+#             return True
+                
+                                
+#         for crs in range(numCourses):
+#             if not dfs(crs):
+#                 return []
+            
+#         return res 
+
+        adj=[[] for i in range(numCourses)]
+        for u,v in prerequisites:
+            adj[v].append(u)
+        # visited=[0]*numCourses
+        indeg=[0]*numCourses
+        res=[]
+        q=deque()
         for i in range(numCourses):
             for j in adj[i]:
-                indeg[j] += 1
+                indeg[j]+=1
         for i in range(numCourses):
-            if indeg[i] == 0:
+            if indeg[i]==0:
                 q.append(i)
-                
         while q:
-            node = q.popleft()
-            res.append(node)
-            for i in adj[node]:
-                if indeg[i] != 0:
-                    indeg[i] -= 1
-                if indeg[i] == 0:
+            u=q.popleft()
+            res.append(u)
+            for i in adj[u]:
+                if indeg[i]!=0:
+                    indeg[i]-=1
+                if indeg[i]==0:
                     q.append(i)
-
-        return res if len(res) == numCourses else []
-
+        if len(res)!=numCourses:
+            return []
+        return res
