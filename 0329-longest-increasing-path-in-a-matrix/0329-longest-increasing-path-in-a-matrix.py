3aae17cb27a7a27baa59e8ee0df2f177cf0420f4
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-04 13:29:20 -0500

    Time: 500 ms (12.55%), Space: 20.9 MB (31.10%) - LeetHub

---
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,56 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+#         rows, cols = len(matrix), len(matrix[0])
+#         if rows == 0:
+#             return 0
+        
+#         indegree = [[0 for x in range(cols)] for y in range(rows)] 
+#         directions = [(0, -1), (0, 1), (1, 0), (-1, 0)]
+        
+#         for x in range(rows):
+#             for y in range(cols):
+#                 for direction in directions:
+#                     nx, ny = x + direction[0], y + direction[1]
+#                     if nx >= 0 and ny >= 0 and nx < rows and ny < cols:
+#                         if matrix[nx][ny] < matrix[x][y]:
+#                             indegree[x][y] += 1
+                            
+#         queue = []
+#         for x in range(rows):
+#             for y in range(cols):
+#                 if indegree[x][y] == 0:
+#                     queue.append((x, y))
+    
+#         path_len = 0
+#         while queue:
+#             sz = len(queue)
+#             for i in range(sz):
+#                 x, y = queue.pop(0)
+#                 for direction in directions:
+#                     nx, ny = x + direction[0], y + direction[1]
+#                     if nx >= 0 and ny >= 0 and nx < rows and ny < cols:
+#                         if matrix[nx][ny] > matrix[x][y]:
+#                             indegree[nx][ny] -= 1
+#                             if indegree[nx][ny] == 0:
+#                                 queue.append((nx, ny))
+#             path_len += 1
+#         return path_len 
+    
+         # dfs
+        def dfs(pre, r, c):
+            # preorder
+            if r in {-1, rows} or c in {-1, cols} or pre >= matrix[r][c]:
+                return 0
+            
+            if (r, c) in vals:
+                return vals[(r, c)]
+            
+            # traverse neighber
+            vals[(r, c)] = 1 + max(dfs(matrix[r][c], r+i, c+j) for (i, j) in dirs)
+            return vals[(r, c)]
+        
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        rows, cols = len(matrix), len(matrix[0])
+        vals = {}
+        
+        return max(dfs(-1, r, c) for r in range(rows) for c in range(cols))
