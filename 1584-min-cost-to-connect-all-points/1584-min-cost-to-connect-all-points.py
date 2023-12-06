c22fb62ea64888d70f00efadbc9157104190b433
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-05 20:51:35 -0500

    Time: 1568 ms (58.00%), Space: 130.6 MB (42.59%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,28 +1,59 @@
+class Edge:
+    def __init__(self, point1, point2, cost):
+        self.point1 = point1
+        self.point2 = point2
+        self.cost = cost
+    def __lt__(self,  other):
+        return self.cost < other.cost # True/False
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [ i for i in range(size)]
+        self.rank = [1] * size
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+            
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        N = len(points)
+        if not points or len(points) == 0:
+            return 0
+        size = len(points)
+        pq = []
+        uf = UnionFind(size)
         
-        adj = { i:[] for i in range(N) } # i : list of [cost, node]
-        
-        for i in range(N):
+        for i in range(size):
             x1, y1 = points[i]
-            for j in range(i + 1, N):
+            for j in range(i+1, size):
                 x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append([dist, j])
-                adj[j].append([dist, i])
+                cost = abs(x1 - x2) + abs(y1 - y2)
+                edge = Edge(i, j, cost)
+                pq.append(edge)
+        heapq.heapify(pq)
+        result = 0
+        count = size - 1
         
-        # Prim's
-        res = 0
-        visit = set()
-        minHeap = [[0, 0]] # [cost, point]
-        while len(visit) < N:
-            cost, i = heapq.heappop(minHeap)
-            if i in visit:
-                continue # if we already visited, skip
-            res += cost
-            visit.add(i)
-            for neiCost, nei in adj[i]:
-                if nei not in visit:
-                    heapq.heappush(minHeap, [neiCost, nei])
-        return res
+        while pq and count > 0:
+            edge = heapq.heappop(pq)
+            if not uf.connected(edge.point1, edge.point2):
+                uf.union(edge.point1, edge.point2)
+                result += edge.cost
+                count -= 1
+        return result
