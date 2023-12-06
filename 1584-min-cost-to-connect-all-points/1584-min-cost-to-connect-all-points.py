3c062ca1f1128396cd8bdd28bc89ca564600e1ba
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-05 23:35:51 -0500

    Time: 1751 ms (52.22%), Space: 130.6 MB (42.73%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -1,23 +1,60 @@
+class Edge:
+    def __init__(self, point1, point2, cost):
+        self.point1 = point1
+        self.point2 = point2
+        self.cost = cost
+    
+    def __lt__(self, other):
+        return self.cost < other.cost
+    
+
+class UnionFind:
+    def __init__(self, size):
+        self.root = [ i for i in range(size) ]
+        self.rank = [1] * size
+    
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootY] < self.rank[rootX]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+                
+
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
-        # Prim
-        size = len(points)
-        visited = set()
-        min_heap = [(0, 0)] # Priority queue to store (cost, vertex) pairs
+        if not points or len(points) == 0:
+            return 0
+        pq = []
+        uf = UnionFind(len(points))
+        
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in range(i+1, len(points)):
+                x2, y2 = points[j]
+                cost = abs(x1-x2) + abs(y1-y2)
+                edge = Edge(i, j, cost)
+                pq.append(edge)
+        heapq.heapify(pq)
         
         result = 0
-        
-        while min_heap:
-            cost, vertex = heapq.heappop(min_heap)
-            
-            if vertex not in visited:
-                visited.add(vertex)
-                
-                result += cost
-                
-                for i in range(size):
-                    if i not in visited:
-                        # calculate Manhattan distance
-                        distance = abs(points[vertex][0] - points[i][0]) + abs(points[vertex][1] - points[i][1])
-                        heapq.heappush(min_heap, (distance, i))
+        count = len(points) - 1
+        while pq and count > 0:
+            edge = heapq.heappop(pq)
+            if not uf.connected(edge.point1, edge.point2):
+                uf.union(edge.point1, edge.point2)
+                result += edge.cost
+                count -= 1
         return result
