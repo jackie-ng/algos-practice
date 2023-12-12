2b02fc1936289b3ea7446840406e1b30498d3261
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-11 20:16:12 -0500

    Time: 1008 ms (71.79%), Space: 20.4 MB (76.88%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -25,9 +25,9 @@ class Solution:
                 else:
                     res.append([value, nums[l], nums[r]])
                     # Update the pointers
-                    #skipping if we found the duplicate of l and we dont need to check 
-					#the duplicate of r cause it will automatically skip the duplicate by the adjustment of l and r
-                    l += 1
+                    # skipping if we found the duplicate of l and we dont need to check (nums[1] = nums[2] = -1)
+					# the duplicate of r cause it will automatically skip the duplicate by the adjustment of l and r
+                    l += 1 # 
                     while nums[l] == nums[l - 1] and l < r:
                         l += 1
         return res
