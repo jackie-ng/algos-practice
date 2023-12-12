dc1dc20c85773614a23d71475a6da5ac2c1e2efb
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-11 20:15:32 -0500

    Time: 961 ms (77.73%), Space: 20.5 MB (34.89%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,10 +2,17 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
+        [-4, -1, -1, 0, 1, 2]
+        [0, 1, 2, 3, 4, 5, 6]
         
         for index, value in enumerate(nums):
             # We don't want to reuse the same value in the same position twice
-            # if it's not the first value in the input array and the value is equal to the value as before => continue to the next iteration for the remaining portion of the array 
+            # if it's not the first value in the input array 
+            # and the value is equal to the value as before (nums[1] = nums[2] = -1)
+            # => continue to the next iteration for the remaining portion of the array 
+            #         [-4, -1, -1, 0, 1, 2]
+            #         [0, 1, 2, 3, 4, 5, 6]
+            #          i  l(i+1)         r(len-1)
             if index > 0 and value == nums[index - 1]:
                 continue
             l, r = index + 1, len(nums) - 1
