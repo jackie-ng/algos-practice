428a04408781300329c377963ebbf4415a12d39e
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-16 17:50:51 -0500

    Time: 32 ms (89.57%), Space: 16.4 MB (15.64%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,6 +1,13 @@
 class Solution:
     def breakPalindrome(self, palindrome: str) -> str:
+        # Greedy approach:
+        # Since it's a palindrome, we can check half of the string
+        # + Split the string in half
+        # + "a" is lexicographically smallest 
+        #### => check if it's equal "a" => replace it with "b" (next lexicographically smallest)
+        #### => Check if it's NOT equal "a" => replace a string to "a"
         n = len(palindrome)
         for i in range(n//2):
-            if palindrome[i] != "a": return palindrome[:i] + "a" + palindrome[i+1:]
+            if palindrome[i] != "a": 
+                return palindrome[:i] + "a" + palindrome[i+1:]
         return palindrome[:-1] + "b" if n > 1 else ""
