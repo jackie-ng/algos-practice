ed50c1aa208aac3ff06c4c931919639ab9528001
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-15 23:01:26 -0500

    Time: 41 ms (37.36%), Space: 16.3 MB (48.76%) - LeetHub

--- 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
@@ -3,7 +3,7 @@ class Solution:
         choices = [9, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         ans, product = 1, 1
         
-        for i in range(n if n <= 10 else 10):
+        for i in range(n):
             product *= choices[i]
             ans += product
             
