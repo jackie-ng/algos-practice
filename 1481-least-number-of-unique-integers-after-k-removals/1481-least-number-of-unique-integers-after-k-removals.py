737f191bf92b99edeeee30d78aacc9bb82e741aa
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2023-12-19 15:34:34 -0500

    Time: 356 ms (89.53%), Space: 33.3 MB (83.86%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -1,19 +1,19 @@
 class Solution:
     def findLeastNumOfUniqueInts(self, arr: List[int], k: int) -> int:
-        # c = Counter(arr)
-        # cnt, remaining = Counter(c.values()), len(c)
-        # for key in range(1, len(arr) + 1): 
-        #     if k >= key * cnt[key]:
-        #         k -= key * cnt[key]
-        #         remaining -= cnt[key]
-        #     else:
-        #         return remaining - k // key
-        # return remaining
+        c = Counter(arr)
+        cnt, remaining = Counter(c.values()), len(c)
+        for key in range(1, len(arr) + 1): # worst case if every element appears exact 1 time
+            if k >= key * cnt[key]:
+                k -= key * cnt[key]
+                remaining -= cnt[key]
+            else:
+                return remaining - k // key
+        return remaining
     
-        hp = list(collections.Counter(arr).values())
-        heapq.heapify(hp)
-        while k > 0:
-            k -= heapq.heappop(hp)
-        # returns the number of unique elements left in the array. 
-        # If k is negative after the last heap pop (which means the last popped count was larger than the remaining k), it adds 1 to the result.
-        return (len(hp) + 1) if k < 0 else len(hp)
+        # hp = list(collections.Counter(arr).values())
+        # heapq.heapify(hp)
+        # while k > 0:
+        #     k -= heapq.heappop(hp)
+        # # returns the number of unique elements left in the array. 
+        # # If k is negative after the last heap pop (which means the last popped count was larger than the remaining k), it adds 1 to the result.
+        # return (len(hp) + 1) if k < 0 else len(hp)
