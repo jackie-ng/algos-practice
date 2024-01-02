75bf40d7fc8ddeb888929f0e619115f10fe194ab
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-02 18:19:49 -0500

    Time: 1234 ms (14.43%), Space: 64.7 MB (15.72%) - LeetHub

--- 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
+++ 1353-maximum-number-of-events-that-can-be-attended/1353-maximum-number-of-events-that-can-be-attended.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxEvents(self, events: List[List[int]]) -> int:
-# Base case for empty list or empty 2d list
+        # Base case for empty list or empty 2d list
         if not events or not events[0]:
             return 0
         sorted_events = list(sorted(events,key=lambda x:x[0]))
@@ -10,30 +10,20 @@ class Solution:
         cur_day = 0
         count = 0
         while i < n or hp:
-            # No day exists in heap, 
-            # Pick current starting day
+            # No day exists in heap, pick current starting day
             if not hp:
                 cur_day = sorted_events[i][0]
-            
-            # Iterate over events that have 
-            # Same starting day as current day
-            # And add their end days to min heap
+            # Iterate over events that have same starting day as current day, and add their end days to min heap
             while i < n and cur_day == sorted_events[i][0]:
                 heappush(hp,sorted_events[i][1])
                 i += 1
-            
             # Pop the earliest ending event
             heappop(hp)
-            
             # Increase count
             count += 1
-            
             # Increase current day (to mark one day of event attended)
             cur_day += 1
-            
-            # If current day has exceeded any 
-            # event end time, we cant attend it, 
-            # Pop it
+            # If current day has exceeded any event end time, we cant attend it => Pop it
             while hp and cur_day > hp[0]:
                 heappop(hp)
             
