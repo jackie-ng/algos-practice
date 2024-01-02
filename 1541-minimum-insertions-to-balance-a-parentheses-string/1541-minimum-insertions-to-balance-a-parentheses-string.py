5f6f9aae0b37b86b5870e34e83cf7bfb43c3dc86
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-02 17:41:12 -0500

    Time: 195 ms (16.02%), Space: 18.2 MB (16.95%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.py
@@ -1,12 +1,27 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
+        # res represents the number of left/right parentheses already added.
+        # right represents the number of right parentheses needed.
         res = right = 0
         for c in s:
+            # 2) case (
+            # If we meet a left parentheses,
+            # we check if we have odd number ')' before.
+            # If we right, we have odd ')' before,
+            # but we want right parentheses in paires.
+            # So add one ')' here, then update right--; res++;.
+            # Note that this part is not necessary if two consecutive right parenthesis not required.
+
+            # Because we have ), we update right += 2.
             if c == '(':
                 if right % 2:
                     right -= 1
                     res += 1
                 right += 2
+            # 1) case )
+            # If we meet a right parentheses , right--.
+            # If right < 0, we need to add a left parentheses before it.
+            # Then we update right += 2 and res++
             if c == ')':
                 right -= 1
                 if right < 0:
@@ -14,21 +29,11 @@ class Solution:
                     res += 1
         return right + res
     
-# res represents the number of left/right parentheses already added.
-# right represents the number of right parentheses needed.
+# Record the number of '(' in an open bracket count int.
+# If there is a '))' or a ') 'then open bracket count -= 1
+# If there is a '))' or a ') and open bracket count = 0 an '(' must be inserted
+# If there is a single ')' another ')' must be inserted
+# At the end of the program and if open bracket count >0 then an '))' must be added for each unmatched '('
 
-# 1) case )
-# If we meet a right parentheses , right--.
-# If right < 0, we need to add a left parentheses before it.
-# Then we update right += 2 and res++
-# This part is easy and normal.
 
-# 2) case (
-# If we meet a left parentheses,
-# we check if we have odd number ')' before.
-# If we right, we have odd ')' before,
-# but we want right parentheses in paires.
-# So add one ')' here, then update right--; res++;.
-# Note that this part is not necessary if two consecutive right parenthesis not required.
 
-# Because we have ), we update right += 2.
