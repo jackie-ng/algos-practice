de629315bba6dccc6b8d46f0e6af6c0169a2c27b
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-04 11:47:43 -0500

    Time: 44 ms (42.20%), Space: 17.3 MB (22.04%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -3,14 +3,19 @@ class Solution:
         res, c = [], Counter(S)
         pq = [(-value,key) for key,value in c.items()]
         heapq.heapify(pq)
+        # previous frequency p_a and previous character p_b
         p_a, p_b = 0, ''
         while pq:
             a, b = heapq.heappop(pq)
             res += [b]
+            # If the previous frequency p_a is less than 0, push the previous character p_b and its frequency back into the heap.
             if p_a < 0:
                 heapq.heappush(pq, (p_a, p_b))
-            a += 1
+            a += 1 # since it's a maxHeap
+            # Update the previous frequency p_a and previous character p_b to a and b respectively.
             p_a, p_b = a, b
         res = ''.join(res)
         if len(res) != len(S): return ""
         return res
+
+  
