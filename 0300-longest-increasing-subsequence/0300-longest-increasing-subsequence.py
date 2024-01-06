321e6358976371c2ff9a832c9db67ac51f5fa7ba
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-05 21:51:14 -0500

    Time: 1804 ms (65.79%), Space: 17.8 MB (14.87%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,42 +1,48 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums:
-            return 0
-        # Initializes an array tails to keep track of the tails of the increasing subsequences.
+        dp = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
+        
+#         if not nums:
+#             return 0
+#         # Initializes an array tails to keep track of the tails of the increasing subsequences.
 
-        tails = [0] * len(nums)
-        length = 0
+#         tails = [0] * len(nums)
+#         length = 0
 
-        # The binary_search_insert function performs binary search to find the position where the current element can be inserted in the increasing subsequence.
-        def binary_search_insert(tails, target, length):
-            low, high = 0, length - 1
+#         # The binary_search_insert function performs binary search to find the position where the current element can be inserted in the increasing subsequence.
+#         def binary_search_insert(tails, target, length):
+#             low, high = 0, length - 1
 
-            while low <= high:
-                mid = (low + high) // 2
-                if tails[mid] == target:
-                    return mid
-                elif tails[mid] < target:
-                    low = mid + 1
-                else:
-                    high = mid - 1
+#             while low <= high:
+#                 mid = (low + high) // 2
+#                 if tails[mid] == target:
+#                     return mid
+#                 elif tails[mid] < target:
+#                     low = mid + 1
+#                 else:
+#                     high = mid - 1
 
-            return low
+#             return low
 
-        # The final result is the length of the LIS.
-        # Time complexity of O(n log n), where n is the length of the input array nums. 
-        # The binary search ensures efficient insertion and updating of elements in the tails array.
-        # For each element in the input array nums, the algorithm finds its position in the tails array using binary search.
-        # If the position is equal to the current length, it means a new element is added, so increment the length.
-        # Otherwise, update the element at that position with the current element.
-        for num in nums:
-            # print(tails)
-            pos = binary_search_insert(tails, num, length)
+#         # The final result is the length of the LIS.
+#         # Time complexity of O(n log n), where n is the length of the input array nums. 
+#         # The binary search ensures efficient insertion and updating of elements in the tails array.
+#         # For each element in the input array nums, the algorithm finds its position in the tails array using binary search.
+#         # If the position is equal to the current length, it means a new element is added, so increment the length.
+#         # Otherwise, update the element at that position with the current element.
+#         for num in nums:
+#             pos = binary_search_insert(tails, num, length)
 
-            tails[pos] = num
-            if pos == length:
-                length += 1
+#             tails[pos] = num
+#             if pos == length:
+#                 length += 1
 
-        return length
+#         return length
     
 
 #         # Dictionary to store memoized results
