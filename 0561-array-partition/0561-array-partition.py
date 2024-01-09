30601812a07648d4f75ff455716561022f6cc529
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-08 22:34:31 -0500

    Time: 220 ms (91.31%), Space: 20.7 MB (25.03%) - LeetHub

--- 0561-array-partition/0561-array-partition.py
+++ 0561-array-partition/0561-array-partition.py
@@ -5,7 +5,7 @@ class Solution:
         # Initialize sum to zero
         max_sum = 0
         for i in range(0, len(nums), 2):
-            # Add every element at even positions (0-indexed)
+            # Add every element at even positions (0-indexed). 2N array
             max_sum += nums[i]
             
         return max_sum
