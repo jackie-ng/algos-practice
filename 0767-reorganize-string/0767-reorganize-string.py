60654717d29eec5d42f1c936066ecaa1b079c5a7
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-10 20:31:54 -0500

    Time: 43 ms (46.30%), Space: 17.4 MB (20.22%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,21 +1,18 @@
 class Solution:
     def reorganizeString(self, S: str) -> str:
         res, c = [], Counter(S)
-        pq = [(-value,key) for key,value in c.items()]
+        pq = [(-value, key) for key, value in c.items()]
         heapq.heapify(pq)
-        # previous frequency p_a and previous character p_b
-        p_a, p_b = 0, ''
+        p_a, p_b = 0, ""
         while pq:
             a, b = heapq.heappop(pq)
             res += [b]
-            # If the previous frequency p_a is less than 0, push the previous character p_b and its frequency back into the heap.
-            if p_a < 0:
+            if p_a < 0: # still has element to pop, add back to the heap
                 heapq.heappush(pq, (p_a, p_b))
-            a += 1 # since it's a maxHeap
-            # Update the previous frequency p_a and previous character p_b to a and b respectively.
-            p_a, p_b = a, b
+            a += 1 # maxHeap => deduct 1 element
+            p_a, p_b = a, b 
         res = ''.join(res)
         if len(res) != len(S): return ""
         return res
-
-  
+            
+  # https://leetcode.com/problems/reorganize-string/discuss/3949276/ex-amazon-explains-a-solution-with-python-javascript-java-and-c/
