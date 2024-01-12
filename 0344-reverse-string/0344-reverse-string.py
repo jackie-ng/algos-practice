2a40c47a389fefd789db49cefc34b9061df8dbce
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-12 15:13:45 -0500

    Time: 178 ms (71.79%), Space: 21.7 MB (18.87%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,10 +5,7 @@ class Solution:
         """
         l, r = 0, len(s) - 1
         while l < r:
-            if s[l] != s[r]:
-                temp = s[l]
-                s[l] = s[r]
-                s[r] = temp
+            s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1 
                 
