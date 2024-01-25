28e2d158cb54991c263a12c4672a0a4d0acf30c6
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-25 16:35:10 -0500

    Time: 32 ms (98.97%), Space: 16.9 MB (83.27%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,18 +1,35 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        memo = {} 
-        def dfs(i, buying):
-            # i reach the end of the array
-            if i >= len(prices):
-                return 0
-            if (i, buying) in memo:
-                return memo[(i, buying)]
-            cooldown = dfs(i+1, buying)
-            if buying:
-                buy = dfs(i+1, not buying) - prices[i]
-                memo[(i, buying)] = max(buy, cooldown)
-            else:
-                sell = dfs(i+2, not buying) + prices[i]
-                memo[(i, buying)] = max(sell, cooldown)
-            return memo[(i, buying)]
-        return dfs(0, True)
+        if not prices:
+            return 0
+
+        n = len(prices)
+
+        # Initialize the states
+        buy = [0] * n
+        sell = [0] * n
+        cooldown = [0] * n
+
+        # Initial values for the first day
+        buy[0] = -prices[0]
+        sell[0] = 0
+        cooldown[0] = 0
+
+        for i in range(1, n):
+
+            # Update buy state
+            buy[i] = max(
+                cooldown[i - 1] - prices[i], # not buy
+                buy[i - 1]) # buy
+
+            # Update sell state
+            sell[i] = max(
+                buy[i - 1] + prices[i], #not sell
+                sell[i - 1]) # sell
+
+            # Update cooldown state
+            cooldown[i] = max(cooldown[i - 1], sell[i - 1])
+
+        # The maximum profit is the maximum of sell and cooldown on the last day
+        return max(sell[n - 1], cooldown[n - 1])
+    
