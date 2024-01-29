a0648d32dc0a30320e775f147f2c1432e7195cd8
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-01-29 14:27:43 -0500

    Time: 46 ms (88.52%), Space: 16.8 MB (64.18%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,37 +1,19 @@
-# # Two Hash Maps
-# class Solution:
-#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-#         # Check for obvious fail case
-#         if len(ransomNote) > len(magazine): return False
-        
-#         magazine_counts = collections.Counter(magazine)
-#         ransom_note_counts = collections.Counter(ransomNote)
-        
-#         # print(magazine_counts)
-#         # print(ransom_note_counts)
-        
-#         # For each *unique* character in the ransom note:
-#         for char, count in ransom_note_counts.items():
-#             # Check that the count of char in the magazine is equal
-#             # or higher than the count in the ransom note.
-#             magazine_count = magazine_counts[char]
-#             if magazine_count < count:
-#                 return False
-            
-#         # if we got this far, we can build the note
-#         return True
-
-# One Hash Map
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        # Check for obvious fail case
+         
+        # Check for obvious fail case.
         if len(ransomNote) > len(magazine): return False
+
+        # In Python, we can use the Counter class. It does all the work that the
+        # makeCountsMap(...) function in our pseudocode did!
         letters = collections.Counter(magazine)
-        
+
+        # For each character, c, in the ransom note:
         for c in ransomNote:
-            # if there are none of c left, return False
-            if letters[c] <= 0: return False
-            # remove one of c from the counter
+            # If there are none of c left, return False.
+            if letters[c] <= 0:
+                return False
+            # Remove one of c from the Counter.
             letters[c] -= 1
-        # if we got this far, we can build the note
+        # If we got this far, we can successfully build the note.
         return True
