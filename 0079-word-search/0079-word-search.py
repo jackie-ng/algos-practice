87c1e2cf25807ebdd586ce8d119f5ef8574aaa70
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-02-15 20:45:45 -0500

    Time: 6176 ms (50.81%), Space: 11.7 MB (78.58%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,23 +1,18 @@
-class Solution:
-    def exist(self, board: List[List[str]], word: str) -> bool:
+class Solution(object):
+    def exist(self, board, word):
         ROWS, COLS = len(board), len(board[0])
         visited = set()
         
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            # check if the range it's inbound and not visited
-            if (r < 0 or r >= ROWS or
-               c < 0 or c >= COLS or
-               word[i] != board[r][c] or (r, c) in visited):
+            if (r < 0 or r >= ROWS or c < 0 or c >= COLS or word[i] != board[r][c] or (r, c) in visited):
                 return False
-            # if it's inbound, add to visited set
             visited.add((r, c))
             res = (dfs(r+1, c, i+1) or
                   dfs(r, c+1, i+1) or
                   dfs(r-1, c, i+1) or
                   dfs(r, c-1, i+1))
-            # clear up for the next traversal
             visited.remove((r, c))
             return res
         for r in range(ROWS):
