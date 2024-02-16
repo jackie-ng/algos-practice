5fc1fd60715c2953f5d98de1abf7d63c293d54ba
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-02-15 21:42:39 -0500

    Time: 895 ms (55.13%), Space: 64 MB (64.60%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -1,13 +1,13 @@
-class Solution:
-    def combine(self, n: int, k: int) -> List[List[int]]:       
-        res  = []
-        def backtrack(start, subset):
+class Solution(object):
+    def combine(self, n, k):
+        res = []
+        def backtrack(i, subset):
             if len(subset) == k:
                 res.append(subset[:])
                 return
-            for num in range(start, n+1):
+            for num in range(i, n+1):
                 subset.append(num)
                 backtrack(num+1, subset)
                 subset.pop()
-        backtrack(1, [])
+        backtrack(1,[])
         return res
