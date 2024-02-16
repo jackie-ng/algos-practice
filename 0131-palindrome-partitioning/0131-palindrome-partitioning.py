f16383b0b2c03e8a7ad727af37c98217059dd0c0
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-02-15 20:54:17 -0500

    Time: 475 ms (83.67%), Space: 30.7 MB (64.92%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,25 +1,22 @@
-class Solution:
-    def partition(self, s: str) -> List[List[str]]:
+class Solution(object):
+    def partition(self, s):
         res = []
         subset = []
-        
         def backtrack(i):
             if i >= len(s):
                 res.append(subset[:])
-                return 
-            
+                return
             for j in range(i, len(s)):
-                if self.isPalidrome(s, i, j):
+                if self.isPalindrome(s, i, j):
                     subset.append(s[i:j+1])
                     backtrack(j+1)
                     subset.pop()
         backtrack(0)
         return res
     
-    def isPalidrome(self, s, l, r):
+    def isPalindrome(self, s, l, r):
         while l < r:
             if s[l] != s[r]:
                 return False
             l, r = l + 1, r - 1
         return True
-      
