14c1f8e80eb2b07ce2a4a045014c8a45f9708692
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-02-16 12:59:46 -0500

    Time: 20 ms (32.44%), Space: 11.8 MB (91.11%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,19 +1,14 @@
-class Solution:
-    def subsets(self, nums: List[int]) -> List[List[int]]:
+class Solution(object):
+    def subsets(self, nums):
         res = []
         subset = []
-        
-        def backtracking(i):
+        def backtrack(i):
             if i >= len(nums):
-                res.append(subset.copy())
+                res.append(subset[:])
                 return
-            
-            # decision to include nums[i]
             subset.append(nums[i])
-            backtracking(i + 1)
-            # decision not to include nums[i]
+            backtrack(i+1)
             subset.pop()
-            backtracking(i + 1)
-            
-        backtracking(0)
+            backtrack(i+1)
+        backtrack(0)
         return res
