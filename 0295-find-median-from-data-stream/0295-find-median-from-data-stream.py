a5d9afdaad3e1f0adb5c2f81145b010c4d26e4e8
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-02-20 16:19:37 -0500

    Time: 362 ms (79.70%), Space: 38 MB (93.51%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,36 +1,40 @@
 class MedianFinder:
+
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        # two heaps, large, small, minheap, maxheap
-        # heaps should be equal size
-        self.small, self.large = [], []  # maxHeap, minHeap (python default)
+        # Initialize two heaps: a max heap (small) and a min heap (large)
+        self.small = []  # Max heap to store the smaller half of elements
+        self.large = []  # Min heap to store the larger half of elements
 
     def addNum(self, num: int) -> None:
+        # Maintain the balance between the two heaps
+
+        # If the number is greater than the smallest element in the large heap
         if self.large and num > self.large[0]:
+            # Add the number to the large heap
             heapq.heappush(self.large, num)
         else:
+            # Add the negation of the number to the small heap (max heap)
             heapq.heappush(self.small, -1 * num)
 
+        # Rebalance the heaps if necessary
+        # Ensure the size difference between the two heaps is at most 1
         if len(self.small) > len(self.large) + 1:
             val = -1 * heapq.heappop(self.small)
             heapq.heappush(self.large, val)
+
         if len(self.large) > len(self.small) + 1:
             val = heapq.heappop(self.large)
             heapq.heappush(self.small, -1 * val)
 
     def findMedian(self) -> float:
+        # Find and return the median of the current elements
+
         if len(self.small) > len(self.large):
+            # If small heap has more elements, return the top element of the small heap
             return -1 * self.small[0]
         elif len(self.large) > len(self.small):
+            # If large heap has more elements, return the top element of the large heap
             return self.large[0]
-        return (-1 * self.small[0] + self.large[0]) / 2.0
-
-        
-
-
-# Your MedianFinder object will be instantiated and called as such:
-# obj = MedianFinder()
-# obj.addNum(num)
-# param_2 = obj.findMedian()
+        else:
+            # If both heaps have equal size, return the average of the top elements
+            return (-1 * self.small[0] + self.large[0]) / 2
