d452f5c52a8447fd15d7554040e44f7bc53de691
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-06 20:28:08 -0500

    Time: 4300 ms (25.91%), Space: 31.7 MB (91.76%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -1,36 +1,21 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         numSet = set(nums)
-        print(numSet)
-        longest = 0
+        res = 0
         
         for n in nums:
-            #check if it's the start of a sequence
-            if (n - 1) not in numSet:
-                length = 0
-                while (n + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
-        return longest
+            # find the start of the sequence
+            if (n-1) not in numSet:
+                # res += 1
+                            
+            # create a variable to hold the range of that sequence --- range = 0
+                range = 0
+                while (n + range) in numSet:
+                # increment the range
+                    range += 1
+                res = max(res, range)
+        return res
                 
-# # sorting
-# class Solution:
-#     def longestConsecutive(self, nums):
-#         if not nums:
-#             return 0
 
-#         nums.sort()
-
-#         longest_streak = 1
-#         current_streak = 1
-
-#         for i in range(1, len(nums)):
-#             if nums[i] != nums[i-1]:
-#                 if nums[i] == nums[i-1]+1:
-#                     current_streak += 1
-#                 else:
-#                     longest_streak = max(longest_streak, current_streak)
-#                     current_streak = 1
-
-#         return max(longest_streak, current_streak)
-
+            # if n + range in numSet (ie 4 + 0, range = 1, 1+1=2)
+            # res = max(res, range)
