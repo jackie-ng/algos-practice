fa93e3518b1dd40d198e7f657b77d4a3ce6cb9d1
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-07 14:21:56 -0500

    Time: 60 ms (11.44%), Space: 16.9 MB (93.53%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,42 +1,12 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # build 2 hash maps: O(s + t), O(1)
         if len(s) != len(t):
             return False
-        
         countS, countT = {}, {}
-        
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0) #0 is a default value if s[i] does not exist in the hashmap
+            countS[s[i]] = 1 + countS.get(s[i], 0)
             countT[t[i]] = 1 + countT.get(t[i], 0)
-        for j in countS:
-            if countS[j] != countT.get(j):
+        for char in s:
+            if countS[char] != countT.get(char):
                 return False
         return True
-            
-#         freq = [0] * 26
-#         for i in range(len(s)):
-#             freq[ord(s[i]) - ord('a')] += 1
-#             freq[ord(t[i]) - ord('a')] -= 1
-        
-#         for i in range(len(freq)):
-#             if freq[i] != 0:
-#                 return False
-        
-#         return True
-    
-#         for c in countS:
-#             if countS[c] != countT[c]:
-#                 return False
-            
-#         return True
-
-           
-    
-    
-    
