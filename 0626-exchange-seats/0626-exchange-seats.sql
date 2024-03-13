1132987915b13ee24cdcf231e0c69443239295a3
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-13 18:07:53 -0400

    Time: 761 ms (33.12%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,37 +1,37 @@
 # Write your MySQL query statement below
-WITH cte AS (
-    SELECT 
-        id, 
-        student,
-        LEAD(id) OVER (ORDER BY id) AS next_seat,
-        LAG(id) OVER (ORDER BY id) AS prev_seat
-    FROM 
-        seat
-)
-SELECT 
-    COALESCE(
-        CASE 
-            WHEN (id % 2 = 1 AND next_seat IS NOT NULL) THEN next_seat
-            WHEN (id % 2 = 0) THEN prev_seat
-            ELSE id 
-        END,
-        id
-    ) AS id, 
-    student
-FROM 
-    cte
-ORDER BY 
-    id;
+# WITH cte AS (
+#     SELECT 
+#         id, 
+#         student,
+#         LEAD(id) OVER (ORDER BY id) AS next_seat,
+#         LAG(id) OVER (ORDER BY id) AS prev_seat
+#     FROM 
+#         seat
+# )
+# SELECT 
+#     COALESCE(
+#         CASE 
+#             WHEN (id % 2 = 1 AND next_seat IS NOT NULL) THEN next_seat
+#             WHEN (id % 2 = 0) THEN prev_seat
+#             ELSE id 
+#         END,
+#         id
+#     ) AS id, 
+#     student
+# FROM 
+#     cte
+# ORDER BY 
+#     id;
     
 
-# SELECT 
-#     CASE 
-#         WHEN id = (SELECT MAX(id) FROM seat) AND id % 2 = 1
-#             THEN id 
-#         WHEN id % 2 = 1
-#             THEN id + 1
-#         ELSE id - 1
-#     END AS id,
-#     student
-# FROM seat
-# ORDER BY id
+SELECT 
+    CASE 
+        WHEN id = (SELECT MAX(id) FROM seat) AND id % 2 = 1
+            THEN id 
+        WHEN id % 2 = 1
+            THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM seat
+ORDER BY id
