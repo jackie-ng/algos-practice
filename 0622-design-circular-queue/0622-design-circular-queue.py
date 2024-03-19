b511bef53d893c51dd509c6b7d5e17f3c558ac45
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-19 19:50:47 -0400

    Time: 69 ms (9.46%), Space: 17.5 MB (5.52%) - LeetHub

--- 0622-design-circular-queue/0622-design-circular-queue.py
+++ 0622-design-circular-queue/0622-design-circular-queue.py
@@ -1,42 +1,39 @@
 class ListNode:
     def __init__(self, val, nxt, prev):
         self.val, self.next, self.prev = val, nxt, prev
+        
 class MyCircularQueue:
-    
+
     def __init__(self, k: int):
         self.space = k
         self.left = ListNode(0, None, None) # left dummy node
-        self.right = ListNode(0, None, self.left) # right dummy node
-        self.left.next = self.right
-
+        self.right = ListNode(0, None, self.left) # right dummy node with prev pointer pointing to left node
+        self.left.next = self.right # left pointer
+        
     def enQueue(self, value: int) -> bool:
         if self.isFull(): return False
-        # assign ListNode with the value and left+right pointers
+        # set value, dummy nodes
         cur = ListNode(value, self.right, self.right.prev)
-   
-        # updating the next attribute of the node that is currently the predecessor of self.right dummy node
-        # it adjusts the next pointer of the node before the right dummy node to point to the newly created node cur. 
-        # Effectively inserts cur into the linked list after the node referenced by self.right.prev.
+        
+        # set pointers 
         self.right.prev.next = cur
-        # updates the prev attribute of self.right to point to the newly created node cur. 
-        # After this line executes, self.right.prev becomes the new last node in the circular queue.
         self.right.prev = cur
         
-        # update the space after enQueue    
+        # reduce space
         self.space -= 1
         return True
-    
+
     def deQueue(self) -> bool:
         if self.isEmpty(): return False
-        # Update the next attribute of the node that is currently the successor of self.left. 
-        # it adjusts the next pointer of the node after the left dummy node to skip the current front node. 
-        # By doing this, it effectively removes the front node from the linked list.
+        
+        # delete front node
         self.left.next = self.left.next.next
-        # updating the prev attribute of the node that is now the new front node in the circular queue. 
-        # It sets the prev pointer of the new front node to point back to the left dummy node. 
-        # This ensures that the circular structure is maintained, and the new front node correctly points to the left dummy node.
+        
+        # set pointer back to left dummy node 
         self.left.next.prev = self.left
+        # increase space
         self.space += 1
+        
         return True
 
     def Front(self) -> int:
@@ -48,8 +45,7 @@ class MyCircularQueue:
         return self.right.prev.val
 
     def isEmpty(self) -> bool:
-        # isEmpty is true when there's only left and right dummy nodes exist
-        return self.left.next == self.right
+        return self.left.next == self.right        
 
     def isFull(self) -> bool:
         return self.space == 0
