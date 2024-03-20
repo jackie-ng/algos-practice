bc11428cb242a0b7001d4bb6acd7be94951cca97
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-19 21:16:37 -0400

    Time: 178 ms (63.52%), Space: 17.4 MB (61.84%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -4,50 +4,33 @@ class Node:
         self.end = end
         self.left = None
         self.right = None
-
-
+        
 class MyCalendar:
+
     def __init__(self):
         self.root = None
 
-    def insert(self, start: int, end: int, node: Node) -> (Node, bool):
+    def insert(self, start:int, end: int, node: Node) -> (Node, bool):
+        # if the calendar is empty => insert the node and its values
         if not node:
-            # base case, insertion is successful
             return Node(start, end), True
+        # if the start OR end is between the interval start/end 
+        # OR  the interval start/end cover the start AND end of the node
         if node.start <= start < node.end or node.start < end <= node.end or (start < node.start and end > node.end):
-            # if the interval to be inserted collides with existing interval, report error
             return node, False
+        
         if start < node.start:
             node.left, success = self.insert(start, end, node.left)
         else:
             node.right, success = self.insert(start, end, node.right)
         return node, success
 
+        
     def book(self, start: int, end: int) -> bool:
         self.root, success = self.insert(start, end, self.root)
         return success
 
 
-# """boundary count based solution ~ 7000 ms"""
-
-
-# from collections import Counter
-
-
-# class MyCalendar:
-#     def __init__(self):
-#         self.delta = Counter()
-
-#     def book(self, start, end) -> bool:
-#         self.delta[start] += 1
-#         self.delta[end] -= 1
-
-#         active = 0
-#         for boundary in sorted(self.delta):
-#             active += self.delta[boundary]
-#             if active > 1:
-#                 # revert the updates
-#                 self.delta[start] -= 1
-#                 self.delta[end] += 1
-#                 return False
-#         return True
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
