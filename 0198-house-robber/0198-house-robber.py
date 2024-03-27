5814f1d8c0f92c5ccf721b0373ec95b5a4373684
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-27 16:58:07 -0400

    Time: 42 ms (16.96%), Space: 16.5 MB (89.66%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,32 +1,28 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
-        
-        # ### Top Down ###
-        # def top_down(i):
-        #     if i >= len(nums):
-        #         return 0
-        #     if i in memo:
-        #         return memo[i]
-        #     memo[i] = max(top_down(i + 1), top_down(i + 2) + nums[i]) # optimal_profit
-        #     return memo[i]
-        # memo = {}
-        # return top_down(0)
-        ### Bottom Up ###
-#         rob1, rob2 = 0, 0
-#         # [rob1, rob2, n, n+1,...]
-#         for n in nums:
-#             rob1, rob2 = rob2, max(rob1+n,rob2)
-#         return rob2
-
+    def robMemo(self, nums: List[int]) -> int:
+        memo = {}
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+            if i in memo:
+                return memo[i]
+            memo[i] = max(dfs(i+1), dfs(i+2) + nums[i])
+            return memo[i]
+        return dfs(0)
+    
+    def robBU(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
-        
         dp = [None] * len(nums)
         dp[0] = nums[0]
-        dp[1] = max(nums[0],nums[1])
+        dp[1] = max(nums[0], nums[1])
         
-        for i in range(2,len(nums)):
-            print(dp)
-            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
-
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
         return max(dp)
+    
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0,0
+        for n in nums:
+            rob1, rob2 = rob2, max(rob1+n, rob2)
+        return rob2
