84b1194300abec8b9686ca7eb12abe83e18bf7a1
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-29 19:55:07 -0400

    Time: 29 ms (87.42%), Space: 16.5 MB (84.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,32 +1,26 @@
 class Solution:
+    def climbStairsMemo(self, n: int) -> int:
+        memo = {}
+        def dfs(i):
+            if i <= 2:
+                return i
+            if i not in memo:
+                memo[i] = dfs(i-1) + dfs(i-2)
+            return memo[i]
+        return dfs(n)
+    
+    def climbStairsBU(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[1] = 1
+        dp[2] = 2
+        
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+    
     def climbStairs(self, n: int) -> int:
-        # Top Down with Memoization
-#         def climb(i):
-#             if i <= 2:
-#                 return i
-#             # Instead of just returning dp(i - 1) + dp(i - 2), calculate it once and then
-#             # store the result inside a hashmap to refer to in the future.
-#             if i not in memo:
-#                 memo[i] = climb(i - 1) + climb(i - 2)
-#             return memo[i]
-        
-#         memo = {}
-#         return climb(n)
-        
-#         # Bottom Up
-#         if n == 1:
-#             return 1
-#         # An array that represents the answer to the problem for a given state
-#         dp = [0] * (n + 1)
-#         # Base case
-#         dp[1] = 1
-#         dp[2] = 2
-        
-#         for i in range(3, n + 1):
-#             dp[i] = dp[i - 1] + dp[i - 2]
-        
-#         return dp[n]
-
         if n == 1:
             return 1
         # Base case
@@ -37,4 +31,3 @@ class Solution:
             one, two = two, one + two
         
         return two
-        
