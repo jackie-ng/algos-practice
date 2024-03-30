55923f4ed3c320db39350646d2e2abe332665fd8
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-29 20:27:25 -0400

    Time: 53 ms (68.40%), Space: 16.7 MB (38.71%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,10 +1,29 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        minimum_cost = [0] * (len(cost) + 1)
-        
+    def minCostClimbingStairsMemo(self, cost: List[int]) -> int:
+        memo = {}
+        def dfs(i):
+            if i <= 1:
+                return 0
+            if i in memo:
+                return memo[i]
+            one = cost[i-1] + dfs(i-1)
+            two = cost[i-2] + dfs(i-2)
+            
+            memo[i] = min(one, two)
+            return memo[i]
+        return dfs(len(cost))
+    
+    def minCostClimbingStairsBU(self, cost: List[int]) -> int:
+        min_cost = [0] * (len(cost) + 1)
         for i in range(2, len(cost) + 1):
-            take_one_step = minimum_cost[i-1] + cost[i-1]
-            take_two_step = minimum_cost[i-2] + cost[i-2]
-            minimum_cost[i] = min(take_one_step, take_two_step)
-        
-        return minimum_cost[-1]
+            one = min_cost[i-1] + cost[i-1]
+            two = min_cost[i-2] + cost[i-2]
+            min_cost[i] = min(one, two)
+        return min_cost[-1]
+    
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        one = two = 0
+        for i in range(2, len(cost) + 1):
+            one, two = min(one + cost[i-1], two + cost[i-2]), one
+        return one
+    
