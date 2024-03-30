45fe8b7c274e980a94d0b87ca22aae3343dd64c6
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-29 20:26:01 -0400

    Time: 32 ms (74.88%), Space: 16.5 MB (85.65%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,10 +1,9 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n < 3:
-            return 1 if n else 0
-        a, b, c = 0, 1, 1
-        
-        for i in range(n - 2):
-            a,b,c = b, c, a+b+c
-        return c
-        
+        memo = {0:0, 1:1, 2:1}
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            memo[i] = dfs(i - 1) + dfs(i - 2) + dfs(i - 3)
+            return memo[i]
+        return dfs(n)
