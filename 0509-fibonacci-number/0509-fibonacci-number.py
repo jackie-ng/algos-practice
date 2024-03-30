d1f5bbe7acf0f14f4d68d1fd8b4ad322932c9598
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-03-29 20:18:05 -0400

    Time: 24 ms (98.60%), Space: 16.5 MB (26.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,5 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    def fibMemo(self, n: int) -> int:
         memo = {}
         def dfs(i):
             if i == 1:
@@ -11,3 +11,13 @@ class Solution:
             memo[i] = dfs(i-1) + dfs(i-2)
             return memo[i]
         return dfs(n)
+    
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        dp = [0] * (n+1)
+        # dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
