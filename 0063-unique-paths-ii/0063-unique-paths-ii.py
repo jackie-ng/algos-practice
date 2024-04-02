ebcc8a63121ca31a0daeba7cebd18ea6e21263d7
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-01 21:14:51 -0400

    Time: 46 ms (45.39%), Space: 16.7 MB (53.14%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,5 +1,5 @@
 class Solution:
-    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+    def uniquePathsWithObstaclesMemo(self, obstacleGrid: List[List[int]]) -> int:
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         memo = {}
         def dfs(i, j):
@@ -15,3 +15,18 @@ class Solution:
             memo[(i, j)] = total
             return total
         return dfs(0,0)
+    
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+        
+        dp[0][0] = 1 if obstacleGrid[0][0] == 0 else 0
+        for i in range(1, m):
+            dp[i][0] = dp[i-1][0] if obstacleGrid[i][0] == 0 else 0
+        for j in range(1, n):
+            dp[0][j] = dp[0][j-1] if obstacleGrid[0][j] == 0 else 0
+        for i in range(1, m):
+            for j in range(1, n):
+                if obstacleGrid[i][j] == 0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[-1][-1]
