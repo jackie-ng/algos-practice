7086a83a56add0fbcb1fbfc995c6985d5919a56b
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-01 20:53:37 -0400

    Time: 28 ms (92.63%), Space: 16.5 MB (42.07%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,5 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
+    def uniquePathsMemo(self, m: int, n: int) -> int:
         memo = {}
         def dfs(i, j):
             if (i, j) in memo:
@@ -12,4 +12,14 @@ class Solution:
             memo[(i, j)] = total
             return total
         return dfs(0,0)
-        
+    
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[0] * n for _ in range(m)]
+        for i in range(m):
+            dp[i][0] = 1
+        for j in range(n):
+            dp[0][j] = 1
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
