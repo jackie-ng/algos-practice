0127045d274ebdf311074381cc651f1ea74d7de8
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-10 15:09:19 -0400

    Time: 36 ms (50.19%), Space: 16.5 MB (41.08%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,11 +1,19 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # https://leetcode.com/problems/word-break-ii/discuss/744674/Diagrammatic-Python-Intuitive-Solution-with-Example
         memo = {}
         def wordsEndingIn(i):
+            # Base case: if i reach the end of the string
             if i == len(s):
                 return [""]
+            # if i is already in memo, return memo[i]
             if i in memo:
                 return memo[i]
+        
+      # - Check all the substring from i to j-1 (exclusive)
+      # - If it's a word, get all possible sentences ending at index 'j' (remaining string)
+      # - If the remaining string is not empty => append current word + " " + remaining string
+      # - If the remaining string is empty => append current word
             ans = []
             for j in range(i+1, len(s)+1):
                 if s[i:j] in wordDict:
