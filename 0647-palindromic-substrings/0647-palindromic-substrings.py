4b3f6d916c94a3b80edd0585e0faf9931a729aba
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-11 12:43:25 -0400

    Time: 226 ms (31.83%), Space: 24.4 MB (19.21%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,21 +1,24 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
-        res = 0
-        for i in range(len(s)):
-            # Odd length palidrome
-            l = r = i
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-            
-            
-            # Even length palidrome
-            l = i 
-            r = i + 1
-            
-            while l >= 0 and r < len(s) and s[l] == s[r]:
-                res += 1
-                l -= 1
-                r += 1
-        return res
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = 0
+        
+        for i in range(n):
+            dp[i][i] = True
+        
+        count = n # each character is a palidromic substring by itself
+        for i in range(n-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+                count += 1
+        
+        
+        for length in range(2, n):
+            for i in range(n-length):
+                j = i+length
+                if s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = True
+                    count += 1
+        
+        return count
