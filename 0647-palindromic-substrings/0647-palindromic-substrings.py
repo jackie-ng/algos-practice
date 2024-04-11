7e6077f85ed5a9340e9f878507706384dfa4b8fe
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-11 12:53:03 -0400

    Time: 161 ms (46.37%), Space: 16.5 MB (88.28%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.py
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -1,5 +1,5 @@
 class Solution:
-    def countSubstrings(self, s: str) -> int:
+    def countSubstringsTabulation(self, s: str) -> int:
         n = len(s)
         dp = [[False] * n for _ in range(n)]
         ans = 0
@@ -8,11 +8,11 @@ class Solution:
             dp[i][i] = True
         
         count = n # each character is a palidromic substring by itself
+        
         for i in range(n-1):
             if s[i] == s[i+1]:
                 dp[i][i+1] = True
                 count += 1
-        
         
         for length in range(2, n):
             for i in range(n-length):
@@ -21,4 +21,20 @@ class Solution:
                     dp[i][j] = True
                     count += 1
         
+        return count
+    
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        for i in range(len(s)):
+            l = r = i
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
+            l = i
+            r = i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                count += 1
+                l -= 1
+                r += 1
         return count
