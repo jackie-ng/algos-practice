018c58007058d52bff41d08af2863f429cf996e2
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-12 15:49:33 -0400

    Time: 413 ms (36.39%), Space: 73 MB (56.40%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,79 +1,76 @@
 class Solution:
-        def numDistinct(self, s: str, t: str) -> int:
-#             # Dictionary for memoization
-#             memo = {}
+    def numDistinctMemo(self, s: str, t: str) -> int:
+        memo = {}
+        def dfs(i, j):
+            """
+            This recursive function performs dynamic programming to count the number of distinct subsequences of string 't' found in string 's'
 
-#             def dfs(i, j):
+            Args:
+              i: Index of the character currently being considered in string 's'
+              j: Index of the character currently being considered in string 't'
 
-#                 # Base case: checks if either i has reached the end of s, j has reached the end of t, 
-#                 # or the remaining length of s is less than the remaining length of t. 
-#                 if i == len(s) or j == len(t) or len(s) - i < len(t) - j:
-#                     return int(j == len(t))
+            Returns:
+              The number of distinct subsequences of 't' found in 's' starting from indices i and j
+            """
 
-#                 # Check if the result is already cached
-#                 if (i, j) in memo:
-#                     return memo[i,j]
+            """ BASE CASE
+            - Reached the end of string 's' (i == len(s))
+            - Reached the end of string 't' (j == len(t))
+            - Remaining length of 's' is less than the remaining length of 't' (insufficient characters in 's' to form the rest of 't')
+            In all these cases, there are zero distinct subsequences, so return 0 (represents an empty subsequence)
+            """
+            if i == len(s) or j == len(t) or len(s) - i < len(t) - j:
+                return int(j == len(t))  # This is True only if j reaches the end of t (meaning the entire t is a subsequence)
 
-#                 # Always make this recursive call
-#                 res = dfs(i + 1, j)
-                
-#                 # If the characters match, add the result to "res"
-#                 # continuing checking
-#                 if s[i] == t[j]:
-#                     res += dfs(i + 1, j + 1)
+            """
+            - If we've already encountered this combination of indices (i, j) while exploring other subsequences,
+          the answer (number of distinct subsequences) is already stored in the memo dictionary.
+            - This avoids redundant calculations by reusing previously found results.
+            """
+            if (i, j) in memo:
+                return memo[(i, j)]
 
-#                 # Cache the answer and return
-#                 memo[i, j] = res
-#                 return res                
+            """ Recursive call considering the next character in 's' (i + 1)
+            - This explores the possibility of excluding the current character (s[i]) from the subsequence.
+            - The recursive call moves forward in 's' (i + 1) but keeps 'j' fixed in 't'.
+            """
+            res = dfs(i + 1, j)
 
-#             return dfs(0, 0)
-    
-#         M, N = len(s), len(t)
-        
-#         # Dynamic Programming table
-#         dp = [[0 for i in range(N + 1)] for j in range(M + 1)] 
-        
-#         # Base case initialization
-#         for j in range(N + 1):
-#             dp[M][j] = 0
-        
-#         # Base case initialization
-#         for i in range(M + 1):
-#             dp[i][N] = 1
-        
-#         # Iterate over the strings in reverse so as to
-#         # satisfy the way we've modeled our recursive solution
-#         for i in range(M - 1, -1, -1):
-#             for j in range(N - 1, -1, -1):
-          
-#                 # Remember, we always need this result
-#                 dp[i][j] = dp[i + 1][j]
+            # If the characters at current indices (i, j) match
+            if s[i] == t[j]:
+                """
+                - If the characters match, there's a possibility that the current character (s[i]) can be part of a subsequence.
+                - Add the result of another recursive call (dfs(i + 1, j + 1)) to 'res'.
+                - This call considers including the current character (s[i]) in the subsequence and moves forward in both strings (i + 1, j + 1).
+                """
+                res += dfs(i + 1, j + 1)
 
-#                 # If the characters match, we add the
-#                 # result of the next recursion call (in this
-#                 # case, the value of a cell in the dp table
-#                 if s[i] == t[j]:
-#                     dp[i][j] += dp[i + 1][j + 1]
+            # Cache the answer for the current subproblem (i, j) and return the final count
+            memo[(i, j)] = res
+            return res
+
+        # Start the dynamic programming process from the beginning (i = 0, j = 0)
+        return dfs(0, 0)
             
-#         return dp[0][0]
-    
-            m = len(s)
-            n = len(t)
-            # Dynamic Programming table
-            dp = [[0] * (n+1) for _ in range(m+1)]
+    def numDistinct(self, s: str, t: str) -> int:
 
-            # Base case initialization
-            for i in range(m+1):
-                dp[i][0] = 1
+        m = len(s)
+        n = len(t)
+        # Dynamic Programming table
+        dp = [[0] * (n+1) for _ in range(m+1)]
 
-            """redundant, as we have initialised dp table with full of zeros"""
-    #         for i in range(1, n+1): 
-    #             dp[0][i] = 0
+        # Base case initialization
+        for i in range(m+1):
+            dp[i][0] = 1
 
-            for i in range(1, m+1):
-                for j in range(1, n+1):
-                    dp[i][j] += dp[i-1][j] 			#if current character is skipped
-                    if s[i-1] == t[j-1]:
-                        dp[i][j] += dp[i-1][j-1]	#if current character is used
+        """redundant, as we have initialised dp table with full of zeros"""
+#         for i in range(1, n+1): 
+#             dp[0][i] = 0
 
-            return dp[-1][-1]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] += dp[i-1][j] 			#if current character is skipped
+                if s[i-1] == t[j-1]:
+                    dp[i][j] += dp[i-1][j-1]	#if current character is used
+
+        return dp[-1][-1]
