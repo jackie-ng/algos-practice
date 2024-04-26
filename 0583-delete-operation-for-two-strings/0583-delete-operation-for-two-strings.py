dd4f936cf947266c321c0233dd94e7d71091283e
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2024-04-26 19:54:19 -0400

    Time: 162 ms (63.89%), Space: 20.6 MB (15.10%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.py
@@ -1,5 +1,5 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
+    def minDistanceMemo(self, word1: str, word2: str) -> int:
         memo = {}
         def dfs(i, j):
             if i < 0 and j < 0:
@@ -19,3 +19,21 @@ class Solution:
                 memo[(i, j)] = min(1+dfs(i, j-1), 1+dfs(i-1, j))
             return memo[(i, j)]
         return dfs(len(word1) - 1, len(word2) - 1)
+    
+    def minDistance(self, word1: str, word2: str) -> int:
+        m, n = len(word1), len(word2)
+        dp = [[0]*(n+1) for _ in range(m+1)]
+        
+        for i in range(1, m+1):
+            dp[i][0] = 1 + dp[i-1][0]
+        for i in range(1, n+1):
+            dp[0][i] = 1 + dp[0][i-1]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if word1[i-1] == word2[j-1]:
+                    dp[i][j] = dp[i-1][j-1]
+                else:
+                    dp[i][j] = min(1+dp[i-1][j], 1+dp[i][j-1])
+        return dp[m][n]
+        
+        
