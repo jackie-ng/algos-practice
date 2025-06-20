91f4eb642ddeb1dbad8de5f22764d0f3d862a709
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-19 23:40:39 -0400

    Time: 1 ms (58.58%), Space: 18.4 MB (87.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,44 +1,22 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        hashmap = {}
-        # # 2 pass
-        # for i in range(len(nums)):
-        #     hashmap[nums[i]] = i
-        # for i in range(len(nums)):
-        #     different = target - nums[i]
-        #     if different in hashmap and hashmap[different] != i:
-        #         return [i, hashmap[different]] 
-            
-        # 1 pass
-        for i in range(len(nums)):
-            different = target - nums[i]
-            if different in hashmap and hashmap[different] != i:
-                return [i, hashmap[different]]
+        copy_nums = nums.copy()
+        left = 0
+        right = len(nums) - 1
+        nums.sort()
+        while left < right:
+            sum = nums[left] + nums[right]
+            if sum == target:
+                break
+            elif sum > target:
+                right -= 1
             else:
-                hashmap[nums[i]] = i
+                left += 1
+        result = []
 
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:        
-#         copy_nums = nums.copy()
-        
-#         left = 0
-#         right = len(nums) - 1
-        
-#         while left < right:
-#             sum = nums[left] + nums[right]
-
-#             if sum == target:
-#                 break
-#             elif sum > target:
-#                 right -= 1
-#             else:
-#                 left += 1
-                
-#         result = []
-#         for i in range(len(nums)):
-#             if nums[left] == copy_nums[i]:
-#                 result.append(i)
-#             elif nums[right] == copy_nums[i]:
-#                 result.append(i)
-                
-#         return result
+        for i in range(len(nums)):
+            if nums[left] == copy_nums[i]:
+                result.append(i)
+            elif nums[right] == copy_nums[i]:
+                result.append(i)
+        return result
