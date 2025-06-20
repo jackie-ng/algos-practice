fc850c005d7d540cfb85c2d365478b3bdb0be77a
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-20 17:04:04 -0400

    Time: 15 ms (42.09%), Space: 17.8 MB (85.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,43 +1,25 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        # build 2 hash maps
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        ## Solution 1: Hashmap
+        # if len(s) != len(t):
+        #     return False
+        # countS, countT = {}, {}
+        # for i in range(len(s)):
+        #     countS[s[i]] = 1 + countS.get(s[i], 0)
+        #     countT[t[i]] = 1 + countT.get(t[i], 0)
+        # for j in countS:
+        #     if countS[j] != countT.get(j, 0):
+        #         return False
+        # return True
+        ## Solution 2: Frequency array
         if len(s) != len(t):
             return False
-        
-        countS, countT = {}, {}
-        
+        freq = [0] * 26
         for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0) #0 is a default value if s[i] does not exist in the hashmap
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        
-        for j in countS:
-            if countS[j] != countT.get(j, 0):
+            freq[ord(s[i]) - ord('a')] += 1
+            freq[ord(t[i]) - ord('a')] -= 1
+        print(freq)
+        for i in range(len(freq)):
+            if freq[i] != 0:
                 return False
         return True
-            
-#         freq = [0] * 26
-#         for i in range(len(s)):
-#             freq[ord(s[i]) - ord('a')] += 1
-#             freq[ord(t[i]) - ord('a')] -= 1
-        
-#         for i in range(len(freq)):
-#             if freq[i] != 0:
-#                 return False
-        
-#         return True
-    
-#         for c in countS:
-#             if countS[c] != countT[c]:
-#                 return False
-            
-#         return True
-
-           
-    
-    
-    
