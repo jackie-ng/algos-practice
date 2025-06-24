ad6f078563033145943df541ceff9cfe0d0403e2
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-24 11:05:46 -0400

    Time: 19 ms (87.5%), Space: 23.3 MB (54.21%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,25 +1,24 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [1] * n
         
-        res = [1] * (len(nums))
+        # Calculate the prefix products
         prefix = 1
+        for i in range(n):
+            answer[i] = prefix    # For index i, answer stores the product of all nums left of i
+            prefix *= nums[i]
         
-        for i in range(len(nums)):
-            res[i] = prefix 
-            prefix *= nums[i] 
-            
-            
-        postfix = 1
-        for i in range(len(nums) - 1, -1, -1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        # Calculate suffix products on the fly and multiply into answer
+        suffix = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= suffix   # Multiply by product of all nums right of i
+            suffix *= nums[i]
         
-#                    [1, 2, 3, 4] 
-# --> prefix = res = [1, 1, 2, 6]
-# --> postfix =      [24, 12, 4, 1]
+        return answer
         
-# lets change 1, 2, 3, 4 positions to symbols like a, b, c, d, so multiplying will be:
+        
+#          lets change 1, 2, 3, 4 positions to symbols like a, b, c, d, so multiplying will be:
 # prefix:
 # ->
 # |       a       |   a*b   | a*b*c | a*b*c*d |
@@ -30,3 +29,17 @@ class Solution:
 # the result is a multiply without the symbol in own position (the left value from prefix and the right one from postfix):
 # |    b*c*d  | a*c*d | a*b*d |   a*b*c   |
 
+# >>> list(range(10))
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# >>> list(range(1, 11))
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# >>> list(range(0, 30, 5))
+# [0, 5, 10, 15, 20, 25]
+# >>> list(range(0, 10, 3))
+# [0, 3, 6, 9]
+# >>> list(range(0, -10, -1))
+# [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+# >>> list(range(0))
+# []
+# >>> list(range(1, 0))
+# []
