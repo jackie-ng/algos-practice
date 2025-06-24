63849698cdf8f9dbaff9248c5846d7b6df9789f9
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-23 23:24:42 -0400

    Time: 11 ms (83.47%), Space: 20.6 MB (89.85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,31 +1,21 @@
-# from collections import defaultdict
-
 class Solution:
-#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-#         res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
-#         print(res)
-#         for s in strs:
-#             count = [0] * 26 # a ... z
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            # Sort the characters in the word to form the key
+            sorted_word = ''.join(sorted(word))
+            anagrams[sorted_word].append(word)
+
+        return list(anagrams.values())
+        # res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
+        # for s in strs:
+        #     count = [0] * 26 # a ... z
             
-#             for c in s:
-#                 count[ord(c) - ord("a")] += 1
+        #     for c in s:
+        #         count[ord(c) - ord("a")] += 1
             
-#             # print(res[tuple(count)], "s:", s)
-#             res[tuple(count)].append(s)
+        #     # print(res[tuple(count)], "s:", s)
+        #     res[tuple(count)].append(s) # list cannot be keys =>  tuple
             
-#         return res.values()
-    
-    def groupAnagrams(self, strs):
-        d = {}
-        
-        for i in range(len(strs)):
-            print(sorted(strs[i]))
-            x = ''.join(sorted(strs[i]))
-            if x not in d:
-                d[x] = [strs[i]]
-            else:
-                d[x].append(strs[i])
-        return d.values()
-    
-# Time Complexity — O(n*len(array item))
-# Space Complexity — O(n)
+        # return list(res.values())
