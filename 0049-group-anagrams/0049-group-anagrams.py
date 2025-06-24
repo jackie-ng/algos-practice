0fc5d6499a7441e857b1f4bdef74f3880a2f6a88
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-23 23:05:01 -0400

    Time: 111 ms (5.05%), Space: 20.2 MB (99.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,31 +1,31 @@
 # from collections import defaultdict
 
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
-        print(res)
-        for s in strs:
-            count = [0] * 26 # a ... z
+#     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+#         res = defaultdict(list) # Defining a dictionary with values as a list (mapping charCount to list of Anagrams)
+#         print(res)
+#         for s in strs:
+#             count = [0] * 26 # a ... z
             
-            for c in s:
-                count[ord(c) - ord("a")] += 1
+#             for c in s:
+#                 count[ord(c) - ord("a")] += 1
             
-            # print(res[tuple(count)], "s:", s)
-            res[tuple(count)].append(s) # list cannot be keys =>  tuple
+#             # print(res[tuple(count)], "s:", s)
+#             res[tuple(count)].append(s)
             
-        return res.values()
+#         return res.values()
     
-#     def groupAnagrams(self, strs):
-#         d = {}
+    def groupAnagrams(self, strs):
+        d = {}
         
-#         for i in range(len(strs)):
-#             print(sorted(strs[i]))
-#             x = ''.join(sorted(strs[i]))
-#             if x not in d:
-#                 d[x] = [strs[i]]
-#             else:
-#                 d[x].append(strs[i])
-#         return d.values()
+        for i in range(len(strs)):
+            print(sorted(strs[i]))
+            x = ''.join(sorted(strs[i]))
+            if x not in d:
+                d[x] = [strs[i]]
+            else:
+                d[x].append(strs[i])
+        return d.values()
     
 # Time Complexity — O(n*len(array item))
 # Space Complexity — O(n)
