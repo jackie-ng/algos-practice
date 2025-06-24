da80ce7413cd56dd70440866fae74e8fd3587526
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-06-23 23:53:19 -0400

    Time: 11 ms (24.41%), Space: 24.2 MB (13.93%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,6 +1,6 @@
+# from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # # from collections import Counter
         # # O(1) time
         # if k == len(nums):
         #     return nums
@@ -10,8 +10,6 @@ class Solution:
         # # 2-3. build heap of top k frequent elements and convert it into an output array
         # # O(N log k) time
         # return heapq.nlargest(k, count.keys(), key=count.get)
-        
-        
         ########## Bucket Sort ############
         # i (count) 0 1 2 3 4 5 <--- go from the end of the freq array back. Freq = length of the input array
         # values     100  2 1    ---> 100 appear 1 time, 2 - 3 times, 1 - 4 times...
