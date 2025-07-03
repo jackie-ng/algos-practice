a4f1628c7e1c1c31705b9d88eb3867dabb380091
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-07-02 22:49:44 -0400

    Time: 0 ms (100%), Space: 18.9 MB (5.35%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,23 +5,28 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # prev, cur = None, head
-        # while cur:
-        #     cur.next, cur, prev = prev, cur.next, cur
-        # return prev
-
-        if not head:
-            return None
+        # # 1. Iterative
+        # curr_node, prev_node = head, None
+        # while curr_node:
+        #     next_node = curr_node.next
+        #     curr_node.next = prev_node
+        #     #update prev_node and curr_node
+        #     prev_node = curr_node
+        #     curr_node = next_node
+        # # prev_node will be pointing at the head of the reversed linked list
+        # return prev_node
+        # 2. Recursive
+        # Base case
+        if (not head) or (not head.next):
+            return head
+        # Recursive case
         cur = head
+        # if we haven't reached the end of the list, run recursion
+        # on node 1, head = 1, head.next = 2, head.next.next = 3
+        # set 2.next = 1 => we have 1->2->1->3
+        # set head.next = None to destroy the pointer => 2->1->3
         if head.next:
             cur = self.reverseList(head.next)
             head.next.next = head
         head.next = None
         return cur
-
-# On Node 1
-# Node for head.next is 2
-# Setting node 2.next = 1 (head.next.next = head)
-# 1->2->1
-# Setting 1-> to null to remove. (head.next = None)
-# 2->1
