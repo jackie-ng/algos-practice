0030ed70b994c716f263f275f9ab9d41381209fa
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-07-10 21:48:03 -0400

    Time: 0 ms (100%), Space: 17.9 MB (35.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,25 +3,34 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+"""
+ 2 4 3 
+ 5 6 4
+ 7 0 8
+
+
+"""
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummyHead = ListNode(0) # for edge case
-        curr = dummyHead
-        carry = 0
-        while l1 != None or l2 != None or carry != 0:
-            l1Val = l1.val if l1 else 0 # value of l1 if l1 is NOT null else, automatically set to 0
-            l2Val = l2.val if l2 else 0
-            
-            # new digit
+        dummy = ListNode()  # Create a dummy node to simplify handling the head of the result list
+        cur = dummy         # Pointer to build the new linked list
+        carry = 0           # Initialize carry for digits summing
+        total = 0           # Initialize total (used inside the loop)
 
-            columnSum = l1Val + l2Val + carry
-            # calculate new carry
-            carry = columnSum // 10
-            newNode = ListNode(columnSum % 10)
-            # update pointers
-            curr.next = newNode
-            # update l1 and l2 pointers if they're not NULL
-            curr = newNode
-            l1 = l1.next if l1 else None
+        while l1 or l2 or carry:  # Continue while either list has nodes left or there is a carry
+            # Get value
+            num1 = l1.val if l1 else 0
+            num2 = l2.val if l2 else 0
+            total = num1 + num2 + carry
+            val = total % 10
+            # Create and append the new node with that digit
+            cur.next = ListNode(val)
+            # Update carry (any value over 9 will carry to next digit)
+            carry = total // 10
+            # Move the current pointer to the new node
+            cur = cur.next
+            # Move to the next node if it's not None
+            l1 = l1.next if l1 else None 
             l2 = l2.next if l2 else None
-        return dummyHead.next
+
+        return dummy.next  # Return the head of the new linked list (skip dummy node)
