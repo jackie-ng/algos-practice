4cac2c097a6e70cf39a4025413f847c494c58270
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-03 18:52:41 -0400

    Time: 9 ms (44.78%), Space: 18 MB (83.1%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,19 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         i = 0
-        j = len(s)-1
-        # a b b a
-        # 0     3
+        j = len(s) - 1
         while i < j:
-            #technically i <= j is gonna be the same
             while i < j and not s[i].isalnum():
-                i+=1
+                i += 1
             while i < j and not s[j].isalnum():
-                j-=1
+                j -= 1
             if s[i].lower() != s[j].lower():
                 return False
-            i+=1
-            j-=1
-        return True
-    #O(N//2) time complexity
-    #O(1) space 
+            i += 1
+            j -= 1
+        return True  
