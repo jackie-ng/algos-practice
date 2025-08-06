d345b80a3d51bddbe9243b72f2d8720fa93612c4
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-05 21:40:11 -0400

    Time: 0 ms (100%), Space: 17.8 MB (65.11%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -1,21 +1,44 @@
+from collections import deque
 class MyStack:
+    # # 2 queues
+    # def __init__(self):
+    #     self.q1 = deque()
+    #     self.q2 = deque()
+    # # Push Operation:
+    # # Move all elements from q1 to q2.
+    # # Add the new element to q1.
+    # # Move all elements back from q2 to q1.
+    # def push(self, x: int) -> None:
+    #     self.q2.append(x)
+    #     while self.q1:
+    #         self.q2.append(self.q1.popleft())
+    #     self.q1, self.q2 = self.q2, self.q1
 
+    # def pop(self) -> int:
+    #     return self.q1.popleft()
+
+    # def top(self) -> int:
+    #     return self.q1[0]
+
+    # def empty(self) -> bool:
+    #     return not self.q1
+    # 1 queue
     def __init__(self):
         self.q = deque()
-
+    
     def push(self, x: int) -> None:
         self.q.append(x)
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
 
     def pop(self) -> int:
-        for i in range(len(self.q) - 1):
-            self.push(self.q.popleft())
         return self.q.popleft()
-    
+
     def top(self) -> int:
-        return self.q[-1]
+        return self.q[0]
 
     def empty(self) -> bool:
-        return len(self.q) == 0
+        return not self.q
 
 
 # Your MyStack object will be instantiated and called as such:
