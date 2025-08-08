535d0b0419929c587e61c87d6c6a26c17a3198a6
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-07 21:07:36 -0400

    Time: 3 ms (87.3%), Space: 18.5 MB (77.8%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,10 +1,16 @@
+"""
+1. Calculate the Total Sum
+2. Iterate with Left Sum: Iterate through array, maintaining a running left sum. 
+For each index, compute the right_sum = total_sum - left_sum - nums[i]
+3. Check Pivot: If left_sum = right_sum => current_index
+"""
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        total = sum(nums)
-        leftSum = 0
+        total_sum = sum(nums)
+        left_sum = 0
         for i in range(len(nums)):
-            rightSum = total - nums[i] - leftSum
-            if leftSum == rightSum:
+            right_sum = total_sum - left_sum - nums[i]
+            if left_sum == right_sum:
                 return i
-            leftSum += nums[i]
-        return -1
+            left_sum += nums[i]
+        return -1        
