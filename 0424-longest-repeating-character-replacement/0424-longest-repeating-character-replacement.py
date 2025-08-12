e4aae1d7754dd9a6cea6b6252d6d4e03242eb3d0
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-11 21:53:55 -0400

    Time: 111 ms (84.82%), Space: 18 MB (49.19%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,19 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        res = 0
-        l = 0
-
-        maxF = 0
-
-        for r in range(len(s)):
-            # for the character at the position r, increment the count of it. If it's already in the hashmap, increase it by 1. If not, set it as 0
-            count[s[r]] = 1 + count.get(s[r], 0)
-            maxF = max(maxF, count[s[r]])
-            # make sure the window is valid using while OR if. We use while in this case
-            while (r - l + 1) - maxF > k:
-                count[s[l]] -= 1
-                l += 1
-                # set the result to the max that it's ever been = the size of the window = r - l + 1 
-            res = max(res, r - l + 1)
-        return res
+        left = 0
+        max_len = 0
+        max_freq = 0
+        for right in range(len(s)):
+            count[s[right]] = count.get(s[right], 0) + 1
+            max_freq = max(max_freq, count[s[right]])
+            if (right - left + 1) - max_freq > k:
+                count[s[left]] -= 1
+                left += 1
+            max_len = max(max_len, right-left+1)
+        return max_len
+            
