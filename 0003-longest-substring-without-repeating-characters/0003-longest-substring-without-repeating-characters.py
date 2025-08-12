b0c1140da62974c23b133de2ed369384b650e036
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-11 21:41:24 -0400

    Time: 11 ms (95.17%), Space: 17.8 MB (52.46%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,17 +1,27 @@
+"""
+abcabcbb 
+    --> longest = abc --> longest length is 3
+
+abcabcbb
+
+c a b
+window = 3
+
+pwwkew
+window = p w w 
+we use while because we want to run until there are no duplicates anymore! 
+"""
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # if not s:
-        #     return 0
-
         max_length = 0
-        start = 0
-        char_index = {}
-
-        for end in range(len(s)):
-            if s[end] in char_index and char_index[s[end]] >= start:
-                start = char_index[s[end]] + 1
-
-            char_index[s[end]] = end
-            max_length = max(max_length, end - start + 1)
-
+        l = 0
+        hash_set = set()
+        for r in range(len(s)):
+            # if s[r] is already seen, shrink the window
+            while s[r] in hash_set:
+                hash_set.remove(s[l])
+                l += 1
+            hash_set.add(s[r])
+            max_length = max(max_length, r-l+1)
         return max_length
+        
