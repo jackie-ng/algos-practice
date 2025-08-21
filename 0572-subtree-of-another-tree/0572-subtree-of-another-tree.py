6358bbe7050a83c6f9b5c82987e1bbda624a6cc0
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-08-20 23:34:28 -0400

    Time: 39 ms (82.56%), Space: 18 MB (83.22%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -4,19 +4,44 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+"""
+Subproblem: LC 100: Same Tree
+"""
 class Solution:
+    # # Recursion
+    # def sameTree(self, a, b):
+    #     if not a and not b:
+    #         return True
+    #     if not a or not b or a.val != b.val:
+    #         return False
+    #     return self.sameTree(a.left, b.left) and self.sameTree(a.right, b.right)
+    # def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+    #     # Recursion
+    #     if not root:
+    #         return False # constraint: subRoot has at least 1 node
+    #     if root.val == subRoot.val and self.sameTree(root, subRoot):
+    #         return True
+    #     return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def sameTree(self, a, b):
+        stack = [(a, b)]
+        while stack:
+            x, y = stack.pop()
+            if not x and not y:
+                continue
+            if not x or not y or x.val != y.val:
+                return False
+            stack.append((x.left, y.left))
+            stack.append((x.right, y.right))
+        return True
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        if not subRoot: return True
-        if not root: return False
-        if self.sameTree(root, subRoot): return True
-        return (self.isSubtree(root.left, subRoot) or \
-                self.isSubtree(root.right, subRoot))
-    
-    def sameTree(self, root, subRoot):
-        if not subRoot and not root: return True
-        
-        if root and subRoot and root.val == subRoot.val:
-            return (self.sameTree(root.left, subRoot.left) and \
-                   self.sameTree(root.right, subRoot.right))
+        stack = [root]
+        target = subRoot.val
+        while stack:
+            node = stack.pop()
+            if node.val == target and self.sameTree(node, subRoot):
+                return True
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
         return False
-        
