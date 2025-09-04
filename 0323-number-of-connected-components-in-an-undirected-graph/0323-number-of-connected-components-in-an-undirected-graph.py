bddc9ff45daead86905c60921192874ac9d9e8cf
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-09-03 21:03:18 -0400

    Time: 2 ms (93.63%), Space: 18.9 MB (75.76%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.py
@@ -1,31 +1,34 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        root = [i for i in range(n)]
-        rank = [1] * n
-        res = n
-        
-        def find(n):
-            if root[n] != n:
-                root[n] = find(root[n])
-            return root[n]
-            
-        def union(n1, n2):
-            r1, r2 = find(n1), find(n2)
-            # if r1 and r2 trees are in the same component
-            if r1 == r2:
-                return 0
-            if rank[r1] > rank[r2]:
-                root[r2] = r1
-            elif rank[r2] > rank[r1]:
-                root[r1] = r2
-            else:
-                root[r1] = r2
-                rank[r2] += 1
-            
-            # if r1 and r2 trees are in the different components
-            return 1
-        
-        for n1, n2 in edges:
-            res -= union(n1, n2)
-        return res
-        
+        # Build adjacency list for 0..n-1
+        graph = [[] for _ in range(n)]
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
+        seen = set()
+        components = 0
+        # # Recursive
+        # def dfs(u):
+        #     seen.add(u)
+        #     for v in graph[u]:
+        #         if v not in seen:
+        #             dfs(v)
+        # for node in range(n):
+        #     if node not in seen:
+        #         components += 1
+        #         dfs(node)
+        # Iterative
+        for start in range(n):
+            if start in seen:
+                continue
+            components += 1
+            # Explicit stack simulating recursion
+            stack = [start]
+            seen.add(start)
+            while stack:
+                u = stack.pop()
+                for v in graph[u]:
+                    if v not in seen:
+                        seen.add(v)
+                        stack.append(v)
+        return components
