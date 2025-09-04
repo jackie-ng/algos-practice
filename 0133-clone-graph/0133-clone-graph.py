2b2b6c13cc6ad5fe33e4d884b2b75d29abb7d291
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-09-03 20:38:12 -0400

    Time: 44 ms (50.76%), Space: 18 MB (82.51%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -5,22 +5,34 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 """
-
-from typing import Optional
 class Solution:
-    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        oldToNew = {}
-        
-        def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node] 
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
-        
-        return dfs(node) if node else None
-        
-                
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        # # Recursive
+        # if not node:
+        #     return None
+        # seen = {}
+        # def dfs(cur):
+        #     if cur in seen:
+        #         return seen[cur]
+        #     copy = Node(cur.val, []) # set the copy node
+        #     seen[cur] = copy # add to the seen hashmap
+        #     for nei in cur.neighbors:
+        #         copy.neighbors.append(dfs(nei))
+        #     return copy
+        # return dfs(node)
+        # Iterative
+        if not node:
+            return None
+        seen = {node: Node(node.val, [])}
+        stack = [node]
+        while stack:
+            cur = stack.pop()
+            cur_copy = seen[cur]
+            for nei in cur.neighbors:
+                if nei not in seen:
+                    seen[nei] = Node(nei.val, [])
+                    stack.append(nei)
+                cur_copy.neighbors.append(seen[nei])
+        return seen[node] 
+#time complexity: O(N+M)
+#space complexity: O(N)
