406e5c37b7be9edb0361f0a8f7d73fbae954a196
Jackie Nguyen (Trang) <88418415+jackie-ng@users.noreply.github.com>
2025-09-27 16:57:28 -0400

    Time: 210 ms (93.35%), Space: 17.7 MB (83.32%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,28 +1,54 @@
+"""
+1) a character is also considered a palindrome. 
+2) even length
+3) odd length 
+palindromic substring with a twist:
+to keep track of the max size of possible substring
+
+
+"""
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        ## Memoization
+        # n = len(s)
+        # if n == 0:
+        #     return ""
+        # @cache
+        # def isPal(i: int, j: int) -> bool:
+        #     if i >= j:
+        #         return True
+        #     if s[i] != s[j]:
+        #         return False
+        #     return isPal(i+1, j-1)
+        # best_i, best_len = 0, 1
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         if isPal(i, j):
+        #             cur_len = j - i + 1
+        #             if cur_len > best_len:
+        #                 best_i, best_len = i, cur_len
+        # return s[best_i : best_i + best_len] 
+        # Expand around center:
         n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        ans = [0, 0]
-        
-        # each character is a palindrome itself
-        for i in range(n):
-            dp[i][i] = True
-        
-        # check for palindrome having length = 2
-        # and then expand from the middle
-        for i in range(n-1):
-            if s[i] == s[i+1]:
-                dp[i][i+1] = True
-                ans = [i, i+1]
-        
-        # check for palindrome with length > 3
-        for length in range(2, n):
-            for i in range(n - length):
-                j = i + length
-                # dp[i+1][j-1] = the start and end of a panlidrome
-                if s[i] == s[j] and dp[i + 1][j - 1]:
-                    dp[i][j] = True
-                    ans = [i, j]
+        if n == 0:
+            return n
+        def expand(l, r):
+            while l >= 0 and r < n and s[l] == s[r]:
+                l -= 1
+                r += 1
+            start = l + 1
+            length = r - l - 1
+            return start, length
 
-        i, j = ans
-        return s[i:j + 1]
+        best_start, best_len = 0, 1
+        for center in range(n):
+            #odd length palindroms
+            start, length = expand(center, center)
+            if length > best_len:
+                best_start, best_len = start, length
+            #even length palindroms
+            start, length = expand(center, center + 1)
+            if length > best_len:
+                best_start, best_len = start, length
+        return s[best_start : best_start + best_len]
